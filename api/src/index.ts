379c5d55bc41097c2f05842f0c7cde4f33cf77f9
Luke Zigler <luke@cautionyourblast.com>
2023-10-05 09:06:01 +0100

    everything prettified

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,24 @@
-import {createServer} from "./server";
+import { createServer } from "./server";
 import config from "config";
-import {logger} from "./services";
+import { logger } from "./services";
 
 async function initApp(): Promise<void> {
-    const server = createServer();
+  const server = createServer();
 
-    server.listen(config.get("port"), () => {
-        logger.info(`Server listening on PORT: ${config.get("port")}, NODE_ENV: ${config.get("env")}`)
-    });
+  server.listen(config.get("port"), () => {
+    logger.info(
+      `Server listening on PORT: ${config.get("port")}, NODE_ENV: ${config.get(
+        "env"
+      )}`
+    );
+  });
 
-    process.on("exit", () => {
-        logger.info(`Server stopped`);
-    });
+  process.on("exit", () => {
+    logger.info(`Server stopped`);
+  });
 }
 
 initApp().catch((err) => {
-    logger.error(["Server initialisation error"], err);
-    process.exit(1);
-})
+  logger.error(["Server initialisation error"], err);
+  process.exit(1);
+});
