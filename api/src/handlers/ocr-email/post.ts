567f001756eb7bcea692efae1d1b95d4dbc4d777
Luke Zigler <luke@cautionyourblast.com>
2023-10-05 11:08:34 +0100

    Added try catch error handling for ocr-email post handler

--- api/src/handlers/ocr-email/post.ts
+++ api/src/handlers/ocr-email/post.ts
@@ -1,5 +1,11 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { HttpException } from "../../middlewares/error-handlers";
 
-export async function post(_req: Request, res: Response) {
-  res.status(200).send("Request successful");
+export async function post(_req: Request, res: Response, next: NextFunction) {
+  try {
+    res.status(200).send("Request successful");
+  } catch (err) {
+    const error = new HttpException(500, "500", (err as Error).message);
+    next(error);
+  }
 }
