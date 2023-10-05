e966ae539a2fa691023f9e2a1e14ee338f4275fc
Luke Zigler <luke@cautionyourblast.com>
2023-10-05 09:19:25 +0100

    Added health check route

--- api/src/routes/initRoutes.ts
+++ api/src/routes/initRoutes.ts
@@ -1,9 +1,22 @@
 import { Express, Router } from "express";
 import { ocrEmailRouter } from "./ocr-email/router";
+import { logger } from "../services";
 
 const router = Router();
 
 router.use("/ocr-email", ocrEmailRouter);
+router.get("/health-check", (_req, res) => {
+  try {
+    res.send({
+      uptime: process.uptime(),
+      message: "OK",
+      timestamp: Date.now(),
+    });
+  } catch (e) {
+    logger.error(["Health check error"], (e as Error).message);
+    res.status(500).send(e);
+  }
+});
 export function initRoutes(server: Express) {
   server.use(router);
 }
