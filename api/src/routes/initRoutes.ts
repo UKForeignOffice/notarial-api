519ec37406e2a52df8841faa5b2a88c929d0c5d6
Luke Zigler <luke@cautionyourblast.com>
2023-10-11 09:37:03 +0100

    Removed try catch from health check route

--- api/src/routes/initRoutes.ts
+++ api/src/routes/initRoutes.ts
@@ -6,16 +6,11 @@ const router = Router();
 
 router.use("/ocr-email", ocrEmailRouter);
 router.get("/health-check", (_req, res) => {
-  try {
-    res.send({
-      uptime: process.uptime(),
-      message: "OK",
-      timestamp: Date.now(),
-    });
-  } catch (e) {
-    logger.error(["Health check error"], (e as Error).message);
-    res.status(500).send(e);
-  }
+  res.send({
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+  });
 });
 export function initRoutes(server: Express) {
   server.use(router);
