c8b5359cc7e58efacaeb22dbf3eb4cda23e7e705
Luke Zigler <luke@cautionyourblast.com>
2023-10-13 15:49:28 +0100

    Added encryption service

---
+++ api/src/middlewares/services/encrypt.ts
@@ -0,0 +1,21 @@
+import config from "config";
+import * as crypto from "crypto";
+
+export class EncryptService {
+  password: string;
+
+  constructor() {
+    this.password = config.get("documentPassword");
+  }
+
+  encryptFile(file: Uint8Array) {
+    const hash = crypto.createHash("sha256");
+    hash.update(this.password);
+    const key = hash.digest();
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv("aes-256-ocb", key, iv);
+    const encryptedFile = cipher.update(file);
+    cipher.final();
+    return encryptedFile;
+  }
+}
