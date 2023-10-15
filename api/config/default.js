8bb028452833d280a3bf14526528edee627a00f6
Luke Zigler <luke@cautionyourblast.com>
2023-10-15 13:55:07 +0100

    Removed S3-related stuff

--- api/config/default.js
+++ api/config/default.js
@@ -1,6 +1,5 @@
 module.exports = {
   port: "9000",
   env: "development",
-  s3Bucket: "",
   documentPassword: "Sup3rS3cr3tP4ssw0rd",
 };
--- api/src/middlewares/services/file.ts
+++ api/src/middlewares/services/file.ts
@@ -1,33 +1,25 @@
-import config from "config";
 import logger, { Logger } from "pino";
 import axios from "axios";
 
 export class FileService {
-  bucketName: string;
   logger: Logger;
 
   constructor() {
-    this.bucketName = config.get("s3Bucket");
-    this.logger = logger().child({ service: "S3" });
+    this.logger = logger().child({ service: "File" });
   }
 
   async getObject(url: string): Promise<ArrayBuffer | void> {
     try {
-      if (!this.bucketName) {
-        this.logger.error(["S001"], "No bucket name has been set");
-        return;
-      }
-
       const fileRes = await axios.get(url);
 
       if (!fileRes.data) {
-        this.logger.error(["S002"], "File could not be found");
+        this.logger.error(["F001"], "File could not be found");
         return;
       }
 
       return await (fileRes.data as Blob).arrayBuffer();
     } catch (e) {
-      this.logger.error(["S003"], `Unknown error: ${(e as Error).message}`);
+      this.logger.error(["F002"], `Unknown error: ${(e as Error).message}`);
       return;
     }
   }
