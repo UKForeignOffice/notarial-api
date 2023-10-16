09b88b63937273b9754dfefeb055233834bd1c4e
Luke Zigler <luke@cautionyourblast.com>
2023-10-16 12:37:38 +0100

    Created helper function for retrieving and encrypting files

---
+++ api/src/handlers/helpers/retrieveAndEncryptFiles.ts
@@ -0,0 +1,30 @@
+import { InputFields } from "./getAllInputsFromForm";
+import { EncryptionService, FileService } from "../../middlewares/services";
+import { MailAttachments } from "../../types/MailAttachments";
+
+export async function retrieveAndEncryptFiles(
+  uploadFields: InputFields,
+  fileService: FileService,
+  encryptionService: EncryptionService
+): Promise<MailAttachments> {
+  const filePromises = Object.values(uploadFields).map(async (url) => {
+    if (!url) {
+      return;
+    }
+    return new Promise(async (resolve, reject) => {
+      const file = await fileService.getObject(url as string);
+      if (!file) {
+        reject(`No file found at url ${url}`);
+      }
+      resolve(encryptionService.encryptFile(file as ArrayBuffer));
+    });
+  });
+  const files = await Promise.all(filePromises);
+  return Object.keys(uploadFields).reduce(
+    (acc, currentKey, currentIndex) => ({
+      ...acc,
+      [currentKey]: files[currentIndex],
+    }),
+    {}
+  );
+}
