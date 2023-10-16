3eddaf88126ed5f49e8337a056499fc98e8ff594
Luke Zigler <luke@cautionyourblast.com>
2023-10-16 12:37:00 +0100

    Added types for email template vars

---
+++ api/src/types/AffirmationTemplate.ts
@@ -0,0 +1,9 @@
+export interface AffirmationTemplate {
+  reference: string;
+  affirmationDetails: string;
+  personalDetails: string;
+  maritalStatus: string;
+  consent: string;
+  declaration: string;
+  paymentDetails: string;
+}
