7f8dfe0d40afc2ca59358384813e409a0157f7a0
Luke Zigler <luke@cautionyourblast.com>
2023-10-19 11:38:29 +0100

    Updated api to send dummy ref back for now

--- api/src/handlers/ocr-email/post.ts
+++ api/src/handlers/ocr-email/post.ts
@@ -3,7 +3,7 @@ import { HttpException } from "../../middlewares/error-handlers";
 
 export async function post(_req: Request, res: Response, next: NextFunction) {
   try {
-    res.status(200).send("Request successful");
+    res.status(200).send({ reference: "PYE-1234" });
   } catch (err) {
     const error = new HttpException(500, "500", (err as Error).message);
     next(error);
