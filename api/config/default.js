b7b6565fb88f0c7003056f58dc8fd9b82e43fb15
Luke Zigler <luke@cautionyourblast.com>
2023-10-23 11:44:02 +0100

    Added dotenv support for config

--- api/config/default.js
+++ api/config/default.js
@@ -1,4 +1,10 @@
-module.exports = {
-    port: "9000",
-    env: "development"
+const dotEnv = require("dotenv");
+
+if (process.env.NODE_ENV !== "test") {
+  dotEnv.config({ path: ".env" });
 }
+
+module.exports = {
+  port: "9000",
+  env: "development",
+};
