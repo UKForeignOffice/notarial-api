ef1ced3c73c3802cfffe6b09b1e060e197ff46e4
Luke Zigler <luke@cautionyourblast.com>
2023-10-26 12:07:34 +0100

    Added buildEmail, sendEmail, and interpolateVars ses functions

--- api/src/middlewares/services/ses.ts
+++ api/src/middlewares/services/ses.ts
@@ -1,9 +1,17 @@
 import logger, { Logger } from "pino";
 import config from "config";
-import { MailAttachments } from "../../types/MailAttachments";
-import { TemplateData } from "../../handlers/helpers/getTemplateDataFromInputs";
-import { GetTemplateCommand, SESClient } from "@aws-sdk/client-ses";
+import {
+  GetTemplateCommand,
+  SendRawEmailCommand,
+  SESClient,
+  Template,
+} from "@aws-sdk/client-ses";
 import { ERRORS } from "../../errors";
+import { createMimeMessage } from "mimetext";
+import {
+  AffirmationTemplateData,
+  CNITemplateData,
+} from "../../handlers/helpers/getTemplateDataFromInputs";
 
 export class SESService {
   templates: {
@@ -38,9 +46,69 @@ export class SESService {
     return Template;
   }
 
-  buildAndSendEmail(
-    template: TemplateData,
-    attachments: MailAttachments,
-    type: "cni" | "affirmation"
-  ) {}
+  buildEmail(
+    template: Template,
+    fields: AffirmationTemplateData | CNITemplateData,
+    uploads: object
+  ) {
+    const message = createMimeMessage();
+    message.setSender({
+      name: "Getting Married Abroad Service",
+      addr: config.get("senderEmail"),
+    });
+    message.setRecipient(config.get("submissionAddress"));
+    try {
+      message.setSubject(this.interpolateVars(template.SubjectPart, fields));
+      for (const [field, file] of Object.entries(uploads)) {
+        message.setAttachment(field, "application/pdf", file.toString());
+      }
+      message.setMessage(
+        "text/html",
+        this.interpolateVars(template.HtmlPart, fields)
+      );
+    } catch (err) {
+      return {
+        errors: err as Error,
+      };
+    }
+    return message.asRaw();
+  }
+
+  async sendEmail(message: string) {
+    try {
+      return await this.ses.send(
+        new SendRawEmailCommand({
+          RawMessage: {
+            Data: Buffer.from(message),
+          },
+        })
+      );
+    } catch (err) {
+      this.handleSESError(err as Error);
+    }
+    return;
+  }
+
+  interpolateVars(string: string = "", vars: object) {
+    if (!string) {
+      this.logger.error(ERRORS.ses.TEMPLATE_PART_MISSING);
+      throw new Error("No string was supplied");
+    }
+    const varsInString = [...string.matchAll(/\{\{[a-zA-Z0-9]*}}/g)];
+    let parsedString = string.slice(0);
+    varsInString.forEach((variable) => {
+      const varRef = variable[0];
+      const varValue = vars[varRef.slice(2, -2)];
+      if (!varValue) {
+        this.logger.error(ERRORS.ses.TEMPLATE_VAR_MISSING);
+        throw new Error(`Missing required variable: ${varRef}`);
+      }
+      parsedString = parsedString.replace(varRef, varValue);
+    });
+    return parsedString;
+  }
+
+  handleSESError(err: Error) {
+    this.logger.error(err);
+  }
 }
