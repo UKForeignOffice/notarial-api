1656c19bb42931297d3dc602bf71d0eab5b0dae3
jen <jen@cautionyourblast.com>
2023-10-30 11:30:42 +0000

    logging

--- api/src/ApplicationError.ts
+++ api/src/ApplicationError.ts
@@ -1,4 +1,5 @@
 import { ErrorCode, ERRORS, ErrorTypes } from "./errors";
+import { HttpStatusCode } from "axios";
 
 type ApplicationErrorOptions = {
   isOperational: boolean;
@@ -11,10 +12,22 @@ const defaultOptions: ApplicationErrorOptions = {
 };
 
 export class ApplicationError extends Error {
+  /**
+   * HTTP status code to send the response as. You may use {@link `Axios#HttpStatusCode`}, or the raw value
+   */
   httpStatusCode: number;
+
+  /**
+   * Notarial-API error code to help identify the error and resolve it
+   */
   code: ErrorCode;
+
   isOperational: boolean = true;
-  exposeToClient: boolean = false;
+
+  /**
+   * determines whether the error message will be included in the response to the client
+   */
+  exposeToClient: boolean = true;
 
   constructor(name: ErrorTypes, code: ErrorCode, httpStatusCode: number, message?: string, options?: Partial<ApplicationErrorOptions>) {
     super(message);
