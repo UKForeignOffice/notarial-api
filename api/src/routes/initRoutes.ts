15af6ed3d9d2606d10cb5ce8ea461d964a5c3e1c
Luke Zigler <luke@cautionyourblast.com>
2023-10-30 12:00:42 +0000

    Removed ApplicationError testing from health-check route

--- api/src/routes/initRoutes.ts
+++ api/src/routes/initRoutes.ts
@@ -1,18 +1,15 @@
 import { Express, Router } from "express";
 import { formRouter } from "./forms/router";
-import { ApplicationError } from "../ApplicationError";
 
 const router = Router();
 
 router.use("/forms", formRouter);
 router.get("/health-check", (_req, res) => {
-  throw new ApplicationError("ses", "TEMPLATE_NOT_FOUND", 500, "some custom message");
-
-  // res.send({
-  //   uptime: process.uptime(),
-  //   message: "OK",
-  //   timestamp: Date.now(),
-  // });
+  res.send({
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+  });
 });
 export function initRoutes(server: Express) {
   server.use(router);
