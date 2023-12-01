c5d5dcaeeee47ba843e1a24b2b5264c2893efddf
Luke Zigler <luke@cautionyourblast.com>
2023-12-01 13:47:13 +0000

    Updated parent EmailService

--- api/src/middlewares/services/EmailService/EmailService.ts
+++ api/src/middlewares/services/EmailService/EmailService.ts
@@ -1,59 +1,17 @@
-import { FormField } from "../../../types/FormField";
 import { SESService } from "./SESService";
 import { NotifyService } from "./NotifyService";
 import logger, { Logger } from "pino";
 
-// export interface EmailService {
-//   sendEmail: (message: string) => Promise<any>;
-//   buildEmailWithAttachments: (params: { subject: string; body: string; attachments: FormField[] }) => Promise<string>;
-//   buildOathEmailBody: (fields: FormField[]) => string;
-//   buildCNIEmailBody: () => void;
-// }
-
-export interface EmailServiceModel {
-  sendEmail: (message: MailModel) => Promise<any>;
-  buildEmail: (formData: FormField[], template: string, serviceType?: "ses" | "notify") => Promise<MailModel>;
-  addAttachments?: (params: { subject: string; body: string; attachments: FormField[] }) => Promise<string>;
-}
-
-export interface NotifyMailModel {
-  template: string;
-  emailAddress: string;
-  options: {
-    personalisation: object;
-    reference: string;
-  };
-}
-
-export type SESMailModel = string;
-
-export type MailModel = NotifyMailModel | SESMailModel;
-
-export type StaffTemplate = "oath" | "cni";
-
-export type UserTemplate = "standard" | "additionalInfoRequired";
-
-export type Template = StaffTemplate | UserTemplate;
-
 export class EmailService {
-  ses: SESService;
-  notify: NotifyService;
   logger: Logger;
+  provider: NotifyService | SESService;
 
-  constructor({ fileService }) {
-    this.logger = logger().child({ service: "Email" });
-    this.ses = new SESService(fileService);
-    this.notify = new NotifyService();
+  constructor(serviceType: string, provider: NotifyService | SESService) {
+    this.logger = logger().child({ service: serviceType });
+    this.provider = provider;
   }
 
-  sendEmail(message: MailModel) {
-    if (typeof message === "string") {
-      return this.ses.sendEmail(message);
-    }
-    return this.notify.sendEmail(message);
-  }
-
-  buildEmail(formData: FormField[], serviceType: "ses" | "notify", template: Template) {
-    this[serviceType].buildEmail(formData, template);
+  sendEmail(args) {
+    return this.provider.send(args);
   }
 }
