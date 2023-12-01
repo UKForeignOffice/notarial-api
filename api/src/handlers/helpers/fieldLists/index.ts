8a0516a91aad66dafe38fb7fe985667ff3bcc8b8
Luke Zigler <luke@cautionyourblast.com>
2023-12-01 13:52:54 +0000

    Removed unnecessary helper functions

--- api/src/handlers/helpers/fieldLists/index.ts
+++
@@ -1,8 +0,0 @@
-import { affirmationFieldList } from "./affirmation";
-export { AffirmationFieldList } from "./affirmation";
-import { cniFieldList } from "./cni";
-
-export const fieldLists = {
-  affirmation: affirmationFieldList,
-  cni: cniFieldList,
-};
