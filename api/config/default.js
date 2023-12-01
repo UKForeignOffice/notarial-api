c7e6edc71489dc5056974a8467394a31a7779595
Luke Zigler <luke@cautionyourblast.com>
2023-12-01 13:53:33 +0000

    Added new env vars to config

--- api/config/default.js
+++ api/config/default.js
@@ -10,4 +10,7 @@ module.exports = {
   documentPassword: "Sup3rS3cr3tP4ssw0rd",
   submissionAddress: "pye@cautionyourblast.com",
   senderEmail: "pye@cautionyourblast.com",
+  notifyApiKey: "",
+  notifyTemplateStandard: "",
+  notifyTemplateAdditionalInfo: "",
 };
