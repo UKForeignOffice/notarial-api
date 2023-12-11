07d621f08c658b4a101459e33788913a9119ee1d
Luke Zigler <luke@cautionyourblast.com>
2023-12-11 11:19:29 +0000

    Updated notify and SES service to handle the building of their own emails

--- api/src/middlewares/services/EmailService/NotifyService.ts
+++ api/src/middlewares/services/EmailService/NotifyService.ts
@@ -2,36 +2,85 @@ import { NotifyClient, RequestError, SendEmailResponse } from "notifications-nod
 import config from "config";
 import pino, { Logger } from "pino";
 import { ApplicationError } from "../../../ApplicationError";
+import { FormField } from "../../../types/FormField";
+import * as additionalContexts from "./additionalContexts.json";
+import { EmailServiceProvider, NotifySendEmailArgs } from "./types";
+import * as templates from "./templates";
 
-export interface NotifyMailModel {
-  template: string;
-  emailAddress: string;
-  options: {
-    personalisation: object;
-    reference: string;
-  };
-}
-export class NotifyService {
+const previousMarriageDocs = {
+  Divorced: "decree absolute",
+  "Dissolved civil partner": "final order",
+  Widowed: "late partner's death certificate",
+  "Surviving civil partner": "late partner's death certificate",
+  Annulled: "decree of nullity",
+};
+export class NotifyService implements EmailServiceProvider {
   notify: NotifyClient;
   logger: Logger;
-  templates = {};
+  templates: Record<string, string>;
   constructor() {
-    this.logger = pino().child({ service: "Notify" });
     const apiKey = config.get("notifyApiKey");
+    const standardTemplate = config.get("notifyTemplateStandard");
     if (!apiKey) {
       throw new ApplicationError("NOTIFY", "NO_API_KEY", 500);
     }
+    if (!standardTemplate) {
+      throw new ApplicationError("NOTIFY", "NO_TEMPLATE", 500);
+    }
+    this.templates = {
+      standard: standardTemplate as string,
+    };
     this.notify = new NotifyClient(apiKey as string);
+    this.logger = pino().child({ service: "Notify" });
   }
 
-  async send({ template, emailAddress, options }: NotifyMailModel) {
+  async send(fields: Record<string, FormField>, template: string, reference: string) {
+    const emailArgs = this.buildSendEmailArgs(fields, template, reference);
+    return this.sendEmail(emailArgs, reference);
+  }
+
+  async sendEmail({ template, emailAddress, options }: NotifySendEmailArgs, reference: string) {
     try {
       const response = await this.notify.sendEmail(template, emailAddress, options);
+      this.logger.info(`Reference ${reference} user email sent successfully with Notify id: ${(response.data as SendEmailResponse).id}`);
       return response.data as SendEmailResponse;
     } catch (e) {
       this.handleError(e);
     }
-    return;
+    throw new ApplicationError("NOTIFY", "API_ERROR", 500, "No data was returned from the api");
+  }
+
+  buildSendEmailArgs(fields: Record<string, FormField>, template: string, reference: string): NotifySendEmailArgs {
+    const defaultTemplate = templates.user[template];
+    const personalisation = this.getPersonalisationForTemplate(fields, reference, fields.paid.answer as boolean, defaultTemplate);
+    return {
+      template: this.templates.standard,
+      emailAddress: fields["emailAddress"].answer as string,
+      options: {
+        personalisation,
+        reference: reference,
+      },
+    };
+  }
+
+  getPersonalisationForTemplate(fields: Record<string, FormField>, reference: string, paid: boolean, template: Record<string, string | boolean>) {
+    const docsList = this.buildDocsList(fields, paid);
+    const country = fields["country"].answer;
+    const post = fields["post"]?.answer;
+    const personalisationValues = {
+      ...fields,
+      docsList,
+      paid,
+      reference,
+      ...additionalContexts[country as string],
+      ...(additionalContexts[post as string] ?? {}),
+    };
+    return Object.keys(template).reduce((acc, curr) => {
+      return {
+        ...acc,
+        [curr]: personalisationValues[curr]?.answer ?? personalisationValues[curr],
+      };
+    }, {});
   }
 
   handleError(error: any) {
@@ -42,5 +91,22 @@ export class NotifyService {
       throw new ApplicationError("NOTIFY", "API_ERROR", 500, JSON.stringify(notifyErrors));
     }
     throw new ApplicationError("NOTIFY", "UNKNOWN", 500, error.message);
+  }
+
+  buildDocsList(fields: Record<string, FormField>, paid: boolean) {
+    let docsList = ["your UK passport", "proof of address", "your partner’s passport or national identity card"];
+    if (fields.marriedBefore?.answer && fields.maritalStatus?.answer) {
+      docsList.push(`your ${previousMarriageDocs[fields.maritalStatus.answer as string]}`);
+    }
+    if (fields.oathType.answer === "affidavit") {
+      docsList.push("religious book of your faith to swear upon");
+    }
+    if (!paid) {
+      docsList.push("the equivalent of £50 in the local currency");
+    }
+    if (additionalContexts[fields.country?.answer as string].additionalDocs) {
+      docsList = docsList.concat(additionalContexts[fields.country?.answer as string].additionalDocs);
+    }
+    return docsList.map((doc) => `* ${doc}`).join("\n");
   }
 }
--- api/src/middlewares/services/EmailService/SESService.ts
+++ api/src/middlewares/services/EmailService/SESService.ts
@@ -3,36 +3,105 @@ import { SendRawEmailCommand, SESClient, SESServiceException } from "@aws-sdk/cl
 import { ses } from "../../../SESClient";
 import * as handlebars from "handlebars";
 import { ApplicationError } from "../../../ApplicationError";
+import { FormField } from "../../../types/FormField";
+import * as templates from "./templates";
+import * as additionalContexts from "./additionalContexts.json";
+import { EmailServiceProvider } from "./types";
+import { createMimeMessage } from "mimetext";
+import config from "config";
+import { FileService } from "../FileService";
+import { getFileFields } from "../helpers";
 
-export class SESService {
+export class SESService implements EmailServiceProvider {
   logger: Logger;
   ses: SESClient;
+  fileService: FileService;
   templates: any;
 
-  constructor() {
+  constructor({ fileService }) {
     this.logger = logger().child({ service: "SES" });
     this.ses = ses;
+    this.fileService = fileService;
+    this.templates = {
+      oath: SESService.createTemplate(templates.staff.oath),
+    };
+  }
+
+  async send(fields: Record<string, FormField>, template: string, reference: string) {
+    const emailArgs = await this.buildSendEmailArgs(fields, template, reference);
+    return this.sendEmail(emailArgs, reference);
   }
 
   /**
    * @throws ApplicationError
    */
-  async send(message: string) {
+  async sendEmail(message: SendRawEmailCommand, reference: string) {
     try {
-      return await this.ses.send(
-        new SendRawEmailCommand({
-          RawMessage: {
-            Data: Buffer.from(message),
-          },
-        })
-      );
+      const response = await this.ses.send(message);
+      this.logger.info(`Reference ${reference} staff email sent successfully with SES message id: ${response.MessageId}`);
+      return response;
     } catch (err: SESServiceException | any) {
       this.logger.error(err);
       throw new ApplicationError("SES", "API_ERROR", 400, err.message);
     }
   }
 
-  static createTemplate(template: string) {
+  getEmailBody(fields: Record<string, FormField>, template: string) {
+    if (template === "cni") {
+      throw new ApplicationError("SES", "TEMPLATE_NOT_FOUND", 500, "CNI template has not been configured");
+    }
+    return this.templates[template]({
+      questions: fields,
+    });
+  }
+
+  async buildSendEmailArgs(fields: Record<string, FormField>, template: string, reference: string): Promise<SendRawEmailCommand> {
+    const emailBody = this.getEmailBody(fields, template);
+    const postField = fields.post;
+    const post = postField?.answer ?? additionalContexts[fields.country.answer as string].post;
+    const message = await this.buildEmailWithAttachments({
+      subject: `${template} | ${post} | ${reference}`,
+      body: emailBody,
+      attachments: getFileFields(fields),
+    });
+    return new SendRawEmailCommand({
+      RawMessage: {
+        Data: Buffer.from(message),
+      },
+    });
+  }
+
+  async buildEmailWithAttachments({ subject, body, attachments }: { subject: string; body: string; attachments: FormField[] }) {
+    const message = createMimeMessage();
+    message.setSender({
+      name: "Getting Married Abroad Service",
+      addr: config.get("senderEmail"),
+    });
+    message.setSubject(subject);
+
+    message.addMessage({
+      contentType: "text/html",
+      data: body,
+    });
+
+    message.setRecipient(config.get("submissionAddress"));
+    try {
+      for (const attachment of attachments) {
+        const { contentType, data } = await this.fileService.getFile(attachment.answer as string);
+        message.addAttachment({
+          filename: attachment.title,
+          contentType,
+          data: data.toString("base64"),
+        });
+      }
+    } catch (err: any) {
+      throw new ApplicationError("SES", "API_ERROR", 400, err.message);
+    }
+
+    return message.asRaw();
+  }
+
+  private static createTemplate(template: string) {
     return handlebars.compile(template);
   }
 }
