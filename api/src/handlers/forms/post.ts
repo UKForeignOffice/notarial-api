49c6f1940c027213af17198b1f86ee3d751f244f
jen <jen@cautionyourblast.com>
2024-01-23 12:25:59 +0000

    debug and errorhandling

--- api/src/handlers/forms/post.ts
+++ api/src/handlers/forms/post.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 export async function post(req: Request, res: Response, next: NextFunction) {
   const { submitService } = res.app.services;
+  req.log.debug(req.params);
   try {
     const { reference } = await submitService.submitForm(req.body);
     res.status(200).send({
