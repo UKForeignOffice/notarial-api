3d921c956406ff1fc662b53b09654c38a9b71e86
jen <jen@cautionyourblast.com>
2024-01-24 12:17:35 +0000

    remove debug line

--- api/src/handlers/forms/post.ts
+++ api/src/handlers/forms/post.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 
 export async function post(req: Request, res: Response, next: NextFunction) {
   const { submitService } = res.app.services;
-  req.log.debug(req.params);
   try {
     const { reference } = await submitService.submitForm(req.body);
     res.status(200).send({
