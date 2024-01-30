3d0998538340284c7cca508f0cd46db8fe39023e
jen <jen@cautionyourblast.com>
2024-01-30 16:50:50 +0000

    fix: getFileFields filters on correct type

--- api/src/middlewares/services/helpers/getFileFields.ts
+++ api/src/middlewares/services/helpers/getFileFields.ts
@@ -2,7 +2,7 @@ import { FormField } from "../../../types/FormField";
 
 export function getFileFields(fields: FormField[]) {
   return fields.reduce<FormField[]>((acc, field) => {
-    if (field.type === "FileUploadField") {
+    if (field.type === "file") {
       return [...acc, field];
     }
     return [...acc];
