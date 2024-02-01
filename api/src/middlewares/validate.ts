fbe23f5169557d09b9becf5157bc6b01fa38d6d7
Jen Duong <jen@cautionyourblast.com>
2024-02-01 17:19:13 +0000

    fix: allow fees (#37)

--- api/src/middlewares/validate.ts
+++ api/src/middlewares/validate.ts
@@ -13,9 +13,10 @@ const webhookOutputSchema = joi.object().keys({
   name: joi.string().required(),
   questions: joi.array().items(questionSchema).required(),
   metadata: joi.object().required(),
+  fees: joi.object().required(),
 });
 export function validationHandler(req: Request, _res: Response, next: NextFunction) {
-  const result = webhookOutputSchema.validate(req.body);
+  const result = webhookOutputSchema.validate(req.body, { abortEarly: false, allowUnknown: true });
   if (result.error) {
     const message = `The supplied form data is invalid: ${result.error.details.map((error) => error.message).join("\n")}`;
     throw new ApplicationError("WEBHOOK", "VALIDATION", 400, message);
