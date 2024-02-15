7af980080ed9072318861fcd812277ec37217cb3
jen <jen@cautionyourblast.com>
2024-02-15 15:25:33 +0000

    rename tests

--- api/src/middlewares/services/EmailService/utils/__tests__/getPostEmailAddress.test.ts
+++ api/src/middlewares/services/EmailService/utils/__tests__/getPostEmailAddress.test.ts
@@ -3,6 +3,6 @@ import { getPostEmailAddress } from "../getPostEmailAddress";
 test("returns the post's email address if it is defined", () => {
   expect(getPostEmailAddress("Albania")).toBe("pye@cautionyourblast.com");
 });
-test("returns the country's email address if the post is not defined", () => {
+test("returns undefined if the post could not be foudn", () => {
   expect(getPostEmailAddress("Turkey", "British Consulate General Istanbul")).toBeUndefined();
 });
