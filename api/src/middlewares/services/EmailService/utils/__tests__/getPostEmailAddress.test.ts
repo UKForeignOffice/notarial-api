ff4cf7265ffa42007c6e61f72e1612d9faf704be
jen <jen@cautionyourblast.com>
2024-02-15 16:54:20 +0000

    typo

--- api/src/middlewares/services/EmailService/utils/__tests__/getPostEmailAddress.test.ts
+++ api/src/middlewares/services/EmailService/utils/__tests__/getPostEmailAddress.test.ts
@@ -3,6 +3,6 @@ import { getPostEmailAddress } from "../getPostEmailAddress";
 test("returns the post's email address if it is defined", () => {
   expect(getPostEmailAddress("Albania")).toBe("pye@cautionyourblast.com");
 });
-test("returns undefined if the post could not be foudn", () => {
+test("returns undefined if the post could not be found", () => {
   expect(getPostEmailAddress("Turkey", "British Consulate General Istanbul")).toBeUndefined();
 });
