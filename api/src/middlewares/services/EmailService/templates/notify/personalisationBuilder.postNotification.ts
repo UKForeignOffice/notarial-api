08d3ec233465890dc8a1d1ac8151db6df3af2b06
jen <jen@cautionyourblast.com>
2024-02-15 18:25:48 +0000

    fix: removing reference from post alert

--- api/src/middlewares/services/EmailService/templates/notify/personalisationBuilder.postNotification.ts
+++ api/src/middlewares/services/EmailService/templates/notify/personalisationBuilder.postNotification.ts
@@ -2,10 +2,9 @@ import { AnswersHashMap } from "../../../../../types/AnswersHashMap";
 import { postNotification } from "./postNotification";
 
 type PostNotification = typeof postNotification;
-export function buildPostNotificationPersonalisation(answers: AnswersHashMap, reference: string): PostNotification {
+export function buildPostNotificationPersonalisation(answers: AnswersHashMap): PostNotification {
   const post = answers["post"] as string;
   return {
     post,
-    reference,
   };
 }
