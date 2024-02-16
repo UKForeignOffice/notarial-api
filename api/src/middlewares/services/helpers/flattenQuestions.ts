5175422569f271ae845175f6c51cf62f43fa1087
jen <jen@cautionyourblast.com>
2024-02-16 16:24:51 +0000

    return category with flattenQuestions

--- api/src/middlewares/services/helpers/flattenQuestions.ts
+++ api/src/middlewares/services/helpers/flattenQuestions.ts
@@ -1,7 +1,7 @@
 import { FormQuestion } from "../../../types/FormQuestion";
 
 export function flattenQuestions(questions: FormQuestion[]) {
-  return questions.flatMap(({ fields }) => {
-    return fields;
+  return questions.flatMap(({ fields, category }) => {
+    return fields.map((field) => ({ ...field, category }));
   });
 }
