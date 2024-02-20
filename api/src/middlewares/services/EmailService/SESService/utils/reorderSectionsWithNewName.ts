30036a3025655ede3a23b83aff57b4f2d6f2a3a5
jen <jen@cautionyourblast.com>
2024-02-20 16:58:09 +0000

    docs

--- api/src/middlewares/services/EmailService/SESService/utils/reorderSectionsWithNewName.ts
+++ api/src/middlewares/services/EmailService/SESService/utils/reorderSectionsWithNewName.ts
@@ -2,6 +2,9 @@ type Reordered<T extends Record<string, string>, P extends object> = {
   [K in keyof T as T[K]]?: P[Extract<keyof P, K>];
 };
 
+/**
+ * Creates a new object with the keys reordered according to the order object.
+ */
 export function reorderSectionsWithNewName<T extends Record<string, string>, P extends object>(order: T) {
   return function (data: Record<string, any>) {
     const newOrder: Reordered<T, P> = {};
