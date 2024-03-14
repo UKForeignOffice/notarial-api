394248487ae03d5650f3d8690788668ec6428bbd
jen <jen@cautionyourblast.com>
2024-03-14 17:34:19 +0000

    feat: add QueueService

--- api/src/errors.ts
+++ api/src/errors.ts
@@ -17,7 +17,7 @@
 /**
  * Category of the error - this is likely to match the service it came from
  */
-export type ErrorTypes = "WEBHOOK" | "SES" | "NOTIFY" | "GENERIC";
+export type ErrorTypes = "WEBHOOK" | "SES" | "NOTIFY" | "QUEUE" | "GENERIC";
 
 /**
  * Error code for the matching ErrorType.
@@ -34,13 +34,14 @@ type SESErrorCode =
   | "MISSING_ANSWER"
   | "UNKNOWN";
 type NotifyErrorCode = "QUEUE_ERROR" | "UNKNOWN";
+type QueueErrorCode = "SES_SEND_ERROR" | "SES_PROCESS_ERROR" | "NOTIFY_SEND_ERROR" | "NOTIFY_PROCESS_ERROR";
 
 type GenericErrorCode = "UNKNOWN" | "RATE_LIMIT_EXCEEDED";
 
 /**
  * Union of all the different ErrorCode.
  */
-export type ErrorCode = WebhookErrorCode | SESErrorCode | NotifyErrorCode | GenericErrorCode;
+export type ErrorCode = WebhookErrorCode | SESErrorCode | NotifyErrorCode | QueueErrorCode | GenericErrorCode;
 
 /**
  * {@ErrorRecord} uses `Record`, which means every key passed into the generic, must be implemented
@@ -76,15 +77,24 @@ const GENERIC: ErrorRecord<GenericErrorCode> = {
   RATE_LIMIT_EXCEEDED: "Rate limit exceeded",
 };
 
+const QUEUE: ErrorRecord<QueueErrorCode> = {
+  NOTIFY_PROCESS_ERROR: "unable to queue NOTIFY_PROCESS_ERROR",
+  NOTIFY_SEND_ERROR: "unable to queue NOTIFY_SEND_ERROR",
+  SES_PROCESS_ERROR: "unable to queue SES_PROCESS_ERROR",
+  SES_SEND_ERROR: "unable to queue SES_SEND_ERROR",
+};
+
 type ErrorRecords = {
   WEBHOOK: typeof WEBHOOK;
   SES: typeof SES;
   NOTIFY: typeof NOTIFY;
+  QUEUE: typeof QUEUE;
   GENERIC: typeof GENERIC;
 };
 export const ERRORS: ErrorRecords = {
   WEBHOOK,
   SES,
   NOTIFY,
+  QUEUE,
   GENERIC,
 };
---
+++ api/src/middlewares/services/QueueService.ts
@@ -0,0 +1,60 @@
+import logger, { Logger } from "pino";
+import PgBoss, { JobOptions, SendOptions } from "pg-boss";
+import config from "config";
+import { ApplicationError } from "../../ApplicationError";
+
+type QueueName = "SES_PROCESS" | "SES_SEND" | "NOTIFY_PROCESS" | "NOTIFY_SEND";
+
+const DEFAULT_OPTIONS = {
+  retryBackoff: config.get<string>("Queue.defaultOptions.retry.backoff") === "true",
+  retryLimit: parseInt(config.get<string>("Queue.defaultOptions.retry.limit")),
+};
+class Queue {
+  options: SendOptions = DEFAULT_OPTIONS;
+  name: QueueName;
+  constructor(name: QueueName, options?: SendOptions) {
+    this.name = name;
+    if (options) {
+      this.options = options;
+    }
+  }
+}
+type JobWithMetadata<T = {}> = T & {
+  metadata: {
+    reference: string;
+  };
+};
+
+export class QueueService {
+  logger: Logger;
+  boss?: PgBoss;
+  queues: Record<QueueName, Queue>;
+
+  constructor() {
+    this.logger = logger().child({ service: "Queue" });
+    const boss = new PgBoss({
+      connectionString: config.get<string>("Queue.url"),
+    });
+    this.queues = {
+      SES_PROCESS: new Queue("SES_PROCESS"),
+      SES_SEND: new Queue("SES_SEND"),
+      NOTIFY_SEND: new Queue("SES_PROCESS"),
+      NOTIFY_PROCESS: new Queue("NOTIFY_PROCESS"),
+    };
+    boss.start().then((pgboss) => {
+      this.boss = pgboss;
+      // this.logger.info(
+      //   `Sending messages to ${this.QUEUE_NAME} or ${this.PROCESS_QUEUE_NAME}. Ensure that there is a handler listening to ${this.QUEUE_NAME} and ${this.PROCESS_QUEUE_NAME}`
+      // );
+    });
+  }
+
+  async sendToQueue(queue: QueueName, data: JobWithMetadata) {
+    const q = this.queues[queue];
+    const jobId = await this.boss?.send?.(queue, data, q.options);
+    if (!jobId) {
+      throw new ApplicationError("QUEUE", `${queue}_ERROR`, 500, `Queueing failed for user: ${data.metadata.reference}`);
+    }
+    return jobId;
+  }
+}
