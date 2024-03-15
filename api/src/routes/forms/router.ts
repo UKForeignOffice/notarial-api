587f64ef120a738e31c55bebbdb28d67bf7dbfde
jen <jen@cautionyourblast.com>
2024-03-15 18:11:50 +0000

    rename handlers

--- api/src/routes/forms/router.ts
+++ api/src/routes/forms/router.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import * as formsHandlers from "../../handlers/forms";
 import * as staffEmailHandlers from "../../handlers/forms/emails/staff";
-import * as notifyEmailHandlers from "../../handlers/forms/emails/user";
+import * as userEmailHandlers from "../../handlers/forms/emails/user";
 import { validationHandler } from "../../middlewares/validate";
 
 export const formRouter = Router();
 
 formRouter.post("/", validationHandler, formsHandlers.post);
 formRouter.post("/emails/ses", staffEmailHandlers.validate, staffEmailHandlers.post);
-formRouter.post("/emails/notify", notifyEmailHandlers.post);
+formRouter.post("/emails/notify", userEmailHandlers.post);
