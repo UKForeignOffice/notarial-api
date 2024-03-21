40b56420f77b0de7a43864388288bd94342cba19
Luke Zigler <luke@cautionyourblast.com>
2024-03-21 11:12:07 +0000

    Added remapper for exchange

---
+++ api/src/middlewares/services/StaffService/mappings/exchange.ts
@@ -0,0 +1,48 @@
+export const order = {
+  applicantDetails: "Applicant's details",
+  applicantAddress: "Applicant's address",
+  maritalStatus: "Marital and civil status",
+  partnerName: "Partner's name",
+  partnerAddress: "Partner's address",
+  partnerNationality: "Partner's nationality",
+  partnerMaritalStatus: "Partner's marital and civil status",
+  marriageDetails: "Marriage",
+  feedbackConsent: "Feedback consent",
+};
+
+export const remap = {
+  country: "information.country",
+  certifyPassport: "information.certifyPassport",
+  feedbackConsent: "feedbackConsent.feedbackConsent",
+
+  firstName: "applicantDetails.firstName",
+  middleName: "applicantDetails.middleName",
+  surname: "applicantDetails.surname",
+  emailAddress: "applicantDetails.emailAddress",
+  phoneNumber: "applicantDetails.phoneNumber",
+
+  addressLine1: "applicantAddress.addressLine1",
+  addressLine2: "applicantAddress.addressLine2",
+  city: "applicantAddress.city",
+  postcode: "applicantAddress.postcode",
+  countryAddress: "applicantAddress.country",
+
+  maritalStatus: "maritalStatus.status",
+
+  partnerFirstName: "partnerName.partnerFirstName",
+  partnerMiddleName: "partnerName.partnerMiddleName",
+  partnerSurname: "partnerName.partnerSurname",
+
+  partnerAddressLine1: "partnerAddress.partnerAddressLine1",
+  partnerAddressLine2: "partnerAddress.partnerAddressLine2",
+  partnerAddressCity: "partnerAddress.partnerAddressCity",
+  partnerAddressPostcode: "partnerAddress.partnerAddressPostcode",
+  partnerAddressCountry: "partnerAddress.partnerAddressCountry",
+
+  partnerMaritalStatus: "partnerMaritalStatus.partnerMaritalStatus",
+
+  partnerNationality: "partnerNationality.partnerNationality",
+
+  dateOfMarriage: "marriageDetails.dateOfMarriage",
+  placeOfMarriage: "marriageDetails.placeOfMarriage",
+};
