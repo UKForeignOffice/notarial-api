5208aed1e1439e7152f92a13c475df67d5e6099a
Luke Zigler <luke@cautionyourblast.com>
2024-04-10 09:44:35 +0100

    Updated getUserTemplate to use upercase values for postal checks

--- api/src/middlewares/services/UserService/getUserTemplate.ts
+++ api/src/middlewares/services/UserService/getUserTemplate.ts
@@ -2,7 +2,7 @@ import * as additionalContexts from "../utils/additionalContexts.json";
 
 export function getUserTemplate(country: string) {
   const postalSupport = additionalContexts.countries[country]?.postal;
-  if (!postalSupport || postalSupport === "false") {
+  if (!postalSupport || postalSupport === "FALSE") {
     return "userConfirmation";
   }
   return "userPostalConfirmation";
