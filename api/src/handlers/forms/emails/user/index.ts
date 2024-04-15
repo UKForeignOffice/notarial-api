24117b8d715f794276a47e3d7ac51fb826f677f5
jen <jen@cautionyourblast.com>
2024-04-15 17:09:00 +0100

    fix: fix validation

--- api/src/handlers/forms/emails/user/index.ts
+++ api/src/handlers/forms/emails/user/index.ts
@@ -1,18 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import joi from "joi";
 import { ApplicationError } from "../../../../ApplicationError";
+
 const schema = joi.object({
-  answers: joi.object(),
+  answers: joi.object().required(),
   metadata: {
-    reference: joi.string(),
+    reference: joi.string().required(),
     payment: joi.object(),
-    type: joi.string().valid("affirmation", "cni", "exchange"),
+    type: joi.string().valid("affirmation", "cni", "exchange").required(),
   },
 });
 export function validate(req: Request, _res: Response, next: NextFunction) {
   const result = schema.validate(req.body, { abortEarly: false, allowUnknown: true });
   if (result.error) {
-    const message = `The supplied form data is invalid: ${result.error.details.map((error) => error.message).join("\n")}`;
+    const message = `The supplied form data is invalid: ${result.error.details.map((error) => error.message).join(",")}`;
     next(new ApplicationError("WEBHOOK", "VALIDATION", 400, message));
   }
   next();
