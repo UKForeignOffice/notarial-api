cc3ccadaeb8a86a19e82e3add1ed25c360f0d085
Luke Zigler <luke@cautionyourblast.com>
2024-04-24 09:42:06 +0100

    Added remapper for postal cni

---
+++ api/src/middlewares/services/StaffService/mappings/cniPostal.ts
@@ -0,0 +1,26 @@
+export const order = {
+  applicantDetails: "Applicant's details",
+  maritalStatus: "Marital and civil status",
+  deliveryDetails: "Delivery details",
+  feedbackConsent: "Feedback consent",
+};
+
+export const remap = {
+  country: "information.country",
+  post: "information.post",
+  certifyPassport: "information.certifyPassport",
+  feedbackConsent: "feedbackConsent.feedbackConsent",
+
+  emailAddress: "applicantDetails.emailAddress",
+  phoneNumber: "applicantDetails.phoneNumber",
+
+  maritalStatus: "maritalStatus.status",
+
+  deliveryFirstName: "deliveryDetails.deliveryFirstName",
+  deliverySurname: "deliveryDetails.deliverySurname",
+  deliveryAddressLine1: "deliveryDetails.deliveryAddressLine1",
+  deliveryAddressLine2: "deliveryDetails.deliveryAddressLine2",
+  deliveryCity: "deliveryDetails.deliveryCity",
+  deliveryPostcode: "deliveryDetails.deliveryPostcode",
+  deliveryPhone: "deliveryDetails.deliveryPhone",
+};
--- api/src/middlewares/services/StaffService/remappers.ts
+++ api/src/middlewares/services/StaffService/remappers.ts
@@ -1,9 +1,11 @@
 import * as affirmationMappings from "./mappings/affirmation";
+import * as cniPostalMappings from "./mappings/cniPostal";
 import * as exchangeMappings from "./mappings/exchange";
 import { createRemapper } from "./utils/createRemapper";
 
 export const remappers = {
   affirmation: createRemapper(affirmationMappings.remap, [], ["file"]),
   cni: createRemapper(affirmationMappings.remap, [], ["file"]),
+  cniPostal: createRemapper(cniPostalMappings.remap, [], ["file"]),
   exchange: createRemapper(exchangeMappings.remap, [], ["file"]),
 };
