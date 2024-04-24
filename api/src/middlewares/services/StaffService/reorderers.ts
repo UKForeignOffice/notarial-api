61945b45d22c30f30519aac951601389eaab954b
Luke Zigler <luke@cautionyourblast.com>
2024-04-24 09:42:24 +0100

    Added reorder for cni postal

--- api/src/middlewares/services/StaffService/reorderers.ts
+++ api/src/middlewares/services/StaffService/reorderers.ts
@@ -1,8 +1,10 @@
 import * as affirmationMappings from "./mappings/affirmation";
+import * as cniPostalMappings from "./mappings/cniPostal";
 import * as exchangeMappings from "./mappings/exchange";
 import { reorderSectionsWithNewName } from "./utils/reorderSectionsWithNewName";
 export const reorderers = {
   affirmation: reorderSectionsWithNewName(affirmationMappings.order),
   cni: reorderSectionsWithNewName(affirmationMappings.order),
+  cniPostal: reorderSectionsWithNewName(cniPostalMappings.order),
   exchange: reorderSectionsWithNewName(exchangeMappings.order),
 };
