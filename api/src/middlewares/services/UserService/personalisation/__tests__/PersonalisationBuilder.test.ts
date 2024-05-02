28508c29fbbf4dfbf89bdcf3a345fa42306eb32a
Luke Zigler <luke@cautionyourblast.com>
2024-05-02 10:43:51 +0100

    Added test for postal confirmation personalisation builder

--- api/src/middlewares/services/UserService/personalisation/__tests__/PersonalisationBuilder.test.ts
+++ api/src/middlewares/services/UserService/personalisation/__tests__/PersonalisationBuilder.test.ts
@@ -3,6 +3,7 @@ import { testData } from "./fixtures";
 import { answersHashMap, flattenQuestions } from "../../../helpers";
 import { PersonalisationBuilder } from "../PersonalisationBuilder";
 import { buildUserConfirmationDocsList } from "../personalisationBuilder.userConfirmation";
+import { getPost } from "../../../utils/getPost";
 const pgBossMock = {
   async start() {
     return this;
@@ -19,7 +20,7 @@ jest.mock("pg-boss", () => {
 const formFields = flattenQuestions(testData.questions);
 const answers = answersHashMap(formFields);
 
-test("buildSendEmailArgs should return the correct personalisation", () => {
+test("buildSendEmailArgs should return the correct personalisation for an in-person email", () => {
   const personalisation = PersonalisationBuilder.userConfirmation(answers, { reference: "1234" });
   expect(personalisation).toEqual({
     firstName: "foo",
@@ -32,6 +33,21 @@ test("buildSendEmailArgs should return the correct personalisation", () => {
     post: "British Consulate General Istanbul",
     confirmationDelay: "2 weeks",
     reference: "1234",
+  });
+});
+
+test("buildSendEmailArgs should return the correct personalisation for a postal email", () => {
+  const personalisation = PersonalisationBuilder.userPostalConfirmation(answers, { reference: "1234" });
+  expect(personalisation).toEqual({
+    firstName: "foo",
+    post: "British Consulate General Istanbul",
+    country: "Turkey",
+    bookingLink: "https://www.book-consular-appointment.service.gov.uk/TimeSelection?location=67&service=10",
+    localRequirements: "",
+    civilPartnership: false,
+    reference: "1234",
+    postAddress: "",
+    notPaid: true,
   });
 });
 
