7a703da5ea8aa3cb5f5a0eb55388d1becd233ee1
Luke Zigler <luke@cautionyourblast.com>
2024-05-08 15:11:40 +0100

    Added new sections to cni postal mapping

--- api/src/middlewares/services/StaffService/mappings/cniPostal.ts
+++ api/src/middlewares/services/StaffService/mappings/cniPostal.ts
@@ -1,6 +1,11 @@
 export const order = {
   applicantDetails: "Applicant's details",
+  applicantAddress: "Applicant's address",
   maritalStatus: "Marital and civil status",
+  partnerName: "Partner's name",
+  partnerAddress: "Partner's address",
+  partnerNationality: "Partner's nationality",
+  partnerMaritalStatus: "Partner's marital and civil status",
   deliveryDetails: "Delivery details",
   feedbackConsent: "Feedback consent",
 };
@@ -11,10 +16,33 @@ export const remap = {
   certifyPassport: "information.certifyPassport",
   feedbackConsent: "feedbackConsent.feedbackConsent",
 
+  firstName: "applicantDetails.firstName",
+  middleName: "applicantDetails.middleName",
+  surname: "applicantDetails.surname",
   emailAddress: "applicantDetails.emailAddress",
   phoneNumber: "applicantDetails.phoneNumber",
 
+  addressLine1: "applicantAddress.addressLine1",
+  addressLine2: "applicantAddress.addressLine2",
+  city: "applicantAddress.city",
+  postcode: "applicantAddress.postcode",
+  countryAddress: "applicantAddress.country",
+
   maritalStatus: "maritalStatus.status",
+
+  partnerFirstName: "partnerName.partnerFirstName",
+  partnerMiddleName: "partnerName.partnerMiddleName",
+  partnerSurname: "partnerName.partnerSurname",
+
+  partnerAddressLine1: "partnerAddress.partnerAddressLine1",
+  partnerAddressLine2: "partnerAddress.partnerAddressLine2",
+  partnerAddressCity: "partnerAddress.partnerAddressCity",
+  partnerAddressPostcode: "partnerAddress.partnerAddressPostcode",
+  partnerAddressCountry: "partnerAddress.partnerAddressCountry",
+
+  partnerNationality: "partnerNationality.partnerNationality",
+
+  partnerMaritalStatus: "partnerMaritalStatus.partnerMaritalStatus",
 
   deliveryFirstName: "deliveryDetails.deliveryFirstName",
   deliverySurname: "deliveryDetails.deliverySurname",
