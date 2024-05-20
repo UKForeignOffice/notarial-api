bf02661e9456f33cfd1e972a0b5b89067cd891c2
jen <jen@cautionyourblast.com>
2024-05-20 16:28:00 +0100

    add test for undefined post with no default

--- api/src/middlewares/services/utils/__tests__/getPostEmailAddress.test.ts
+++ api/src/middlewares/services/utils/__tests__/getPostEmailAddress.test.ts
@@ -3,9 +3,15 @@ import { getPostEmailAddress } from "../getPostEmailAddress";
 test("returns the post's email address if it is defined", () => {
   expect(getPostEmailAddress("Albania")).toBe("pye+albania@cautionyourblast.com");
 });
+
 test("returns undefined if the post could not be found", () => {
   expect(getPostEmailAddress("Turkey", "British Consulate General Istanbul")).toBeUndefined();
 });
+
 test("returns a post email address if the country has multiple posts but no post was passed in", () => {
   expect(getPostEmailAddress("Russia")).toBe("pye+moscow@cautionyourblast.com");
+});
+
+test("returns undefined the post is undefined, and there is no default for that country", () => {
+  expect(getPostEmailAddress("France")).toBeUndefined();
 });
