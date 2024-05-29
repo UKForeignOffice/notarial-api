bdd00ec9e7fed40a6366e7f21c1c2eef47c95cd8
Luke Zigler <luke@cautionyourblast.com>
2024-05-29 10:15:02 +0200

    Added remappers and reorderers for msc and cni msc combined

---
+++ api/src/middlewares/services/StaffService/mappings/CNIAndMSC.ts
@@ -0,0 +1,60 @@
+export const order = {
+  applicantDetails: "Applicant's details",
+  applicantAddress: "Applicant's address",
+  applicantPassport: "Applicant's passport",
+  maritalStatus: "Marital and civil status",
+  partnerName: "Partner's name",
+  partnerAddress: "Partner's address",
+  partnerNationality: "Partner's nationality",
+  partnerMaritalStatus: "Partner's marital and civil status",
+  deliveryDetails: "Delivery details",
+  feedbackConsent: "Feedback consent",
+};
+
+export const remap = {
+  country: "information.country",
+  post: "information.post",
+  certifyPassport: "information.certifyPassport",
+  feedbackConsent: "feedbackConsent.feedbackConsent",
+
+  firstName: "applicantDetails.firstName",
+  middleName: "applicantDetails.middleName",
+  surname: "applicantDetails.surname",
+  dateOfBirth: "applicantDetails.dateOfBirth",
+  sex: "applicantDetails.sex",
+  emailAddress: "applicantDetails.emailAddress",
+  phoneNumber: "applicantDetails.phoneNumber",
+
+  addressLine1: "applicantAddress.addressLine1",
+  addressLine2: "applicantAddress.addressLine2",
+  city: "applicantAddress.city",
+  postcode: "applicantAddress.postcode",
+  countryAddress: "applicantAddress.country",
+
+  passportDateOfIssue: "applicantPassport.dateOfIssue",
+  passportNumber: "applicantPassport.number",
+
+  maritalStatus: "maritalStatus.status",
+
+  partnerFirstName: "partnerName.partnerFirstName",
+  partnerMiddleName: "partnerName.partnerMiddleName",
+  partnerSurname: "partnerName.partnerSurname",
+
+  partnerAddressLine1: "partnerAddress.partnerAddressLine1",
+  partnerAddressLine2: "partnerAddress.partnerAddressLine2",
+  partnerAddressCity: "partnerAddress.partnerAddressCity",
+  partnerAddressPostcode: "partnerAddress.partnerAddressPostcode",
+  partnerAddressCountry: "partnerAddress.partnerAddressCountry",
+
+  partnerNationality: "partnerNationality.partnerNationality",
+
+  partnerMaritalStatus: "partnerMaritalStatus.partnerMaritalStatus",
+
+  deliveryFirstName: "deliveryDetails.deliveryFirstName",
+  deliverySurname: "deliveryDetails.deliverySurname",
+  deliveryAddressLine1: "deliveryDetails.deliveryAddressLine1",
+  deliveryAddressLine2: "deliveryDetails.deliveryAddressLine2",
+  deliveryCity: "deliveryDetails.deliveryCity",
+  deliveryPostcode: "deliveryDetails.deliveryPostcode",
+  deliveryPhone: "deliveryDetails.deliveryPhone",
+};
--- api/src/middlewares/services/StaffService/reorderers.ts
+++ api/src/middlewares/services/StaffService/reorderers.ts
@@ -1,10 +1,14 @@
 import * as affirmationMappings from "./mappings/affirmation";
 import * as cniPostalMappings from "./mappings/cniPostal";
 import * as exchangeMappings from "./mappings/exchange";
+import * as mscMappings from "./mappings/msc";
+import * as cniMSCCombinedMappings from "./mappings/CNIAndMSC";
 import { reorderSectionsWithNewName } from "./utils/reorderSectionsWithNewName";
 export const reorderers = {
   affirmation: reorderSectionsWithNewName(affirmationMappings.order),
   cni: reorderSectionsWithNewName(affirmationMappings.order),
   cniPostal: reorderSectionsWithNewName(cniPostalMappings.order),
   exchange: reorderSectionsWithNewName(exchangeMappings.order),
+  msc: reorderSectionsWithNewName(mscMappings.order),
+  "cni and msc": reorderSectionsWithNewName(cniMSCCombinedMappings.order),
 };
