0894ca8181925e78c6b51ec5f599e3fc6ec9dfc6
Luke Zigler <luke@cautionyourblast.com>
2024-06-03 09:23:05 +0100

    Updated getPost tests

--- api/src/middlewares/services/utils/__tests__/getPost.test.ts
+++ api/src/middlewares/services/utils/__tests__/getPost.test.ts
@@ -1,16 +1,16 @@
 import { getPost } from "../getPost";
 
 test("getPost returns the correct post for countries with one post", () => {
-  expect(getPost("Albania")).toBe("British Embassy Tirana");
-  expect(getPost("Algeria")).toBe("British Embassy Algiers");
-  expect(getPost("Austria")).toBe("British Embassy Vienna");
+  expect(getPost("Albania")).toBe("the British Embassy Tirana");
+  expect(getPost("Algeria")).toBe("the British Embassy Algiers");
+  expect(getPost("Austria")).toBe("the British Embassy Vienna");
 });
 
 test("getPost returns the correct post if the post is specified", () => {
-  expect(getPost("Turkey", "British Consulate General Istanbul")).toBe("British Consulate General Istanbul");
-  expect(getPost("Turkey", "British Embassy Ankara")).toBe("British Embassy Ankara");
-  expect(getPost("Vietnam", "British Embassy Hanoi")).toBe("British Embassy Hanoi");
-  expect(getPost("Vietnam", "British Consulate General Ho Chi Minh City")).toBe("British Consulate General Ho Chi Minh City");
+  expect(getPost("Turkey", "the British Consulate General Istanbul")).toBe("the British Consulate General Istanbul");
+  expect(getPost("Turkey", "the British Embassy Ankara")).toBe("the British Embassy Ankara");
+  expect(getPost("Vietnam", "the British Embassy Hanoi")).toBe("the British Embassy Hanoi");
+  expect(getPost("Vietnam", "the British Consulate General Ho Chi Minh City")).toBe("the British Consulate General Ho Chi Minh City");
 });
 
 test("getPost returns the correct post if the country has multiple posts but a post has been specified", () => {
