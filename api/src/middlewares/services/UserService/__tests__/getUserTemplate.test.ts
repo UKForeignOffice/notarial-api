0e554802df0c186d6b9ff5462f5e6d4f3a64f383
Luke Zigler <luke@cautionyourblast.com>
2024-06-11 10:58:25 +0100

    Updated getUserTemplate tests

--- api/src/middlewares/services/UserService/__tests__/getUserTemplate.test.ts
+++ api/src/middlewares/services/UserService/__tests__/getUserTemplate.test.ts
@@ -12,13 +12,21 @@ jest.mock("../../utils/additionalContexts.json", () => {
 });
 
 test("Returns postal template if the form has the metadata value postal: true", () => {
-  expect(getUserTemplate("Brazil", true)).toBe("userPostalConfirmation");
+  expect(getUserTemplate("Brazil", true, "cni")).toBe("userPostalConfirmation");
 });
 
 test("Returns in-person template if the form has the metadata value postal: false", () => {
-  expect(getUserTemplate("Turkey", false)).toBe("userConfirmation");
+  expect(getUserTemplate("Turkey", false, "cni")).toBe("userConfirmation");
 });
 
-test("Returns correct postal value from country if the form has no postal metadata value", () => {
-  expect(getUserTemplate("Turkey")).toBe("userPostalConfirmation");
+test("Returns correct postal value from country if the form has no postal metadata value and is using the exchange journey", () => {
+  expect(getUserTemplate("Turkey", undefined, "exchange")).toBe("userPostalConfirmation");
+});
+
+test("Returns correct postal value from country if the form has no postal value and the user is submitting an affirmation", () => {
+  expect(getUserTemplate("Turkey", undefined, "affirmation")).toBe("userConfirmation");
+});
+
+test("Returns correct postal value from country if the form has no postal metadata, but the user is submitting a cni", () => {
+  expect(getUserTemplate("Sweden", undefined, "cni")).toBe("userConfirmation");
 });
