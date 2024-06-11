147fd7d46be7bab2a8eed6f1a74f6d4dd9c5d8e7
Luke Zigler <luke@cautionyourblast.com>
2024-06-11 11:01:21 +0100

    Fixed ts error

--- api/src/middlewares/services/UserService/getUserTemplate.ts
+++ api/src/middlewares/services/UserService/getUserTemplate.ts
@@ -1,7 +1,7 @@
 import * as additionalContexts from "../utils/additionalContexts.json";
 import { FormType } from "../../../types/FormDataBody";
 
-export function getUserTemplate(country: string, postal?: boolean, type: FormType) {
+export function getUserTemplate(country: string, type: FormType, postal?: boolean) {
   const postalSupport = postal ?? (type === "exchange" && additionalContexts.countries[country]?.postal);
   if (!postalSupport) {
     return "userConfirmation";
