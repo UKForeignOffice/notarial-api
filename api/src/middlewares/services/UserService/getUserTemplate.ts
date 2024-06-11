bb6d4b464fbb5c6345438c2a5a1bc7c0df4129cd
Luke Zigler <luke@cautionyourblast.com>
2024-06-11 10:58:11 +0100

    Updated getUserTemplate to check form type as well when deciding the template

--- api/src/middlewares/services/UserService/getUserTemplate.ts
+++ api/src/middlewares/services/UserService/getUserTemplate.ts
@@ -1,7 +1,8 @@
 import * as additionalContexts from "../utils/additionalContexts.json";
+import { FormType } from "../../../types/FormDataBody";
 
-export function getUserTemplate(country: string, postal?: boolean) {
-  const postalSupport = postal ?? additionalContexts.countries[country]?.postal;
+export function getUserTemplate(country: string, postal?: boolean, type: FormType) {
+  const postalSupport = postal ?? (type === "exchange" && additionalContexts.countries[country]?.postal);
   if (!postalSupport) {
     return "userConfirmation";
   }
