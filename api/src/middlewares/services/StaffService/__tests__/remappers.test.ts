56833ec9e520ec9c4047c2836c299d08acfbdb54
Luke Zigler <luke@cautionyourblast.com>
2024-07-16 10:36:17 +0100

    Added snapshot tests for all remappers

--- api/src/middlewares/services/StaffService/__tests__/remappers.test.ts
+++ api/src/middlewares/services/StaffService/__tests__/remappers.test.ts
@@ -5,3 +5,23 @@ test("affirmation is successfully remapped", () => {
   const remapped = remappers.affirmation(fields);
   expect(remapped).toMatchSnapshot();
 });
+
+test("CNIAndMSC is successfully remapped", () => {
+  const remapped = remappers.cniAndMscPostal(fields);
+  expect(remapped).toMatchSnapshot();
+});
+
+test("cniPostal is successfully remapped", () => {
+  const remapped = remappers.cniPostal(fields);
+  expect(remapped).toMatchSnapshot();
+});
+
+test("exchange is successfully remapped", () => {
+  const remapped = remappers.exchange(fields);
+  expect(remapped).toMatchSnapshot();
+});
+
+test("msc is successfully remapped", () => {
+  const remapped = remappers.mscPostal(fields);
+  expect(remapped).toMatchSnapshot();
+});
