24836fe01c0147c505e2784af1b004ee187ac446
Luke Zigler <luke@cautionyourblast.com>
2024-07-17 10:32:29 +0100

    Moved birth cert condition till after proof of address so that the docs are ordered correctly

--- api/src/middlewares/services/UserService/personalisation/personalisationBuilder.userConfirmation.ts
+++ api/src/middlewares/services/UserService/personalisation/personalisationBuilder.userConfirmation.ts
@@ -44,20 +44,21 @@ export function buildUserConfirmationDocsList(fields: AnswersHashMap, type?: For
 
   const docsList = ["your UK passport", "your partner’s passport or national identity card"];
 
+  // for cni applications, proof of address is only required if the user lives in the country. For affirmation applications, proof of address is always required
+  if (type === "affirmation" || (type === "cni" && fields.livesInCountry)) {
+    docsList.splice(1, 0, "proof of address – you must use your residence permit if the country you live in issues these");
+  }
+
+  // for cnis, the user needs to provide proof they have stayed in the country for 3 days. For contextual reasons, this should appear below the proof of address doc
+  if (type === "cni" && !fields.livesInCountry) {
+    docsList.splice(1, 0, "proof you’ve been staying in the country for 3 whole days before your appointment – if this is not shown on your proof of address");
+  }
+
   // for affirmations, users need to provide their birth certificate. For contextual reasons, this should appear next to the user's passport
   if (type === "affirmation") {
     docsList.splice(1, 0, "your birth certificate");
   }
 
-  // for cni applications, proof of address is only required if the user lives in the country. For affirmation applications, proof of address is always required
-  if (type === "affirmation" || (type === "cni" && fields.livesInCountry)) {
-    docsList.splice(2, 0, "proof of address – you must use your residence permit if the country you live in issues these");
-  }
-
-  // for cnis, the user needs to provide proof they have stayed in the country for 3 days. For contextual reasons, this should appear below the proof of address doc
-  if (type === "cni" && !fields.livesInCountry) {
-    docsList.splice(2, 0, "proof you’ve been staying in the country for 3 whole days before your appointment – if this is not shown on your proof of address");
-  }
   if (fields.maritalStatus && fields.maritalStatus !== "Never married") {
     docsList.push(`${previousMarriageDocs[fields.maritalStatus as string]}`);
   }
