9e79b443ec08cff53071078c32a08c57b6ffae3d
Luke Zigler <luke@cautionyourblast.com>
2024-07-17 11:33:45 +0100

    removed default post for Italy as it's now added in the dynamic content

--- api/src/middlewares/services/utils/getPost.ts
+++ api/src/middlewares/services/utils/getPost.ts
@@ -1,12 +1,5 @@
 import * as additionalContexts from "./additionalContexts.json";
 
-/**
- * Some countries have two posts, but we need to provide a default post for users to send their documents to.
- */
-const defaultPostForPostal = {
-  Italy: additionalContexts.posts["the British Embassy Rome"].post,
-};
-
 export function getPost(country: string, post?: string) {
-  return post ?? additionalContexts.countries?.[country]?.post ?? defaultPostForPostal[country];
+  return post ?? additionalContexts.countries?.[country]?.post;
 }
