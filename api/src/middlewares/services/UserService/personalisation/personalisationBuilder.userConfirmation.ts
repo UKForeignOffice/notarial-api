a274e8042b0e479288302dd0e617774766d6aec2
Luke Zigler <luke@cautionyourblast.com>
2024-07-22 15:35:19 +0100

    Removed docs list logic

--- api/src/middlewares/services/UserService/personalisation/personalisationBuilder.userConfirmation.ts
+++ api/src/middlewares/services/UserService/personalisation/personalisationBuilder.userConfirmation.ts
@@ -16,7 +16,6 @@ export function buildUserConfirmationPersonalisation(answers: AnswersHashMap, me
     throw new ApplicationError("WEBHOOK", "VALIDATION", 500, "Fields are empty");
   }
   const additionalPersonalisations = personalisationTypeMap[metadata.type as string]?.(answers) ?? {};
-  // const docsList = buildUserConfirmationDocsList(answers, metadata.type);
   const country = answers["country"] as string;
   const post = answers["post"] as string;
 
@@ -55,37 +54,3 @@ export function getCNIPersonalisations(fields: AnswersHashMap) {
     religious: fields.oathType === "Religious",
   };
 }
-
-// export function buildUserConfirmationDocsList(fields: AnswersHashMap, type?: FormType) {
-//   if (!fields) {
-//     throw new ApplicationError("WEBHOOK", "VALIDATION", 500, "Fields are empty");
-//   }
-//
-//   const docsList = ["your UK passport", "your partner’s passport or national identity card"];
-//
-//   // for cni applications, proof of address is only required if the user lives in the country. For affirmation applications, proof of address is always required
-//   if (type === "affirmation" || (type === "cni" && fields.livesInCountry)) {
-//     docsList.splice(1, 0, "proof of address – you must use your residence permit if the country you live in issues these");
-//   }
-//
-//   // for cnis, the user needs to provide proof they have stayed in the country for 3 days. For contextual reasons, this should appear below the proof of address doc
-//   if (type === "cni" && !fields.livesInCountry) {
-//     docsList.splice(1, 0, "proof you’ve been staying in the country for 3 whole days before your appointment – if this is not shown on your proof of address");
-//   }
-//
-//   // for affirmations, users need to provide their birth certificate. For contextual reasons, this should appear next to the user's passport
-//   if (type === "affirmation") {
-//     docsList.splice(1, 0, "your birth certificate");
-//   }
-//
-//   if (fields.maritalStatus && fields.maritalStatus !== "Never married") {
-//     docsList.push(`${previousMarriageDocs[fields.maritalStatus as string]}`);
-//   }
-//   if (fields.oathType === "Religious") {
-//     docsList.push("religious book of your faith to swear upon");
-//   }
-//   const country = fields.country as string;
-//   const additionalDocs = additionalContexts.countries[country]?.additionalDocs ?? [];
-//   docsList.push(...additionalDocs);
-//   return docsList.map((doc) => `* ${doc}`).join("\n");
-// }
