4c6488c427c1d9c47c67478dc60fc032216c2f46
Luke Zigler <luke@cautionyourblast.com>
2024-07-29 10:36:32 +0100

    updated getUserTemplate for Croatia specific changes

--- api/src/middlewares/services/UserService/getUserTemplate.ts
+++ api/src/middlewares/services/UserService/getUserTemplate.ts
@@ -2,7 +2,14 @@ import * as additionalContexts from "../utils/additionalContexts.json";
 import { FormType } from "../../../types/FormDataBody";
 
 export function getUserTemplate(country: string, type: FormType, postal?: boolean) {
-  const postalSupport = postal ?? (type === "exchange" && additionalContexts.countries[country]?.postal && additionalContexts.countries[country]?.cniDelivery);
+  // for exchange forms, any country that offers a postal journey and cni delivery should be a postal application.
+  const countryOffersPostalRoute = additionalContexts.countries[country]?.postal && additionalContexts.countries[country]?.cniDelivery;
+
+  // Croatia is an exception to this, and only offers in-person applications for exchange
+  const countryIsCroatia = country === "Croatia";
+
+  const postalSupport = postal ?? (type === "exchange" && countryOffersPostalRoute && !countryIsCroatia);
+
   if (!postalSupport) {
     return "userConfirmation";
   }
