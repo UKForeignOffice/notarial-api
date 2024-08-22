6cc0a66611177ebd95ad3c3a01a2f6f8e96e0f1c
Jen Duong <jen@cautionyourblast.com>
2024-08-22 16:31:16 +0100

    fix: italy croatia emails (#131)
    * remove additional docs function since this template does not use them
    
    * remove import
    
    * fix email
    
    * update content

--- api/src/middlewares/services/UserService/personalisation/personalisationBuilder/userConfirmation/getAdditionalDocsForCountry.ts
+++ api/src/middlewares/services/UserService/personalisation/personalisationBuilder/userConfirmation/getAdditionalDocsForCountry.ts
@@ -5,7 +5,7 @@ type Metadata = { type: FormType; reference: string; payment?: PayMetadata };
 
 export const getAdditionalDocsForCountry = {
   Montenegro: (_answers: AnswersHashMap, _additionalContext, metadata: Metadata) => {
-    if (metadata.type === "affirmation") {
+    if (metadata?.type === "affirmation") {
       return [];
     }
     return ["UK CNI issued by a UK register office"];
--- api/src/middlewares/services/UserService/personalisation/personalisationBuilder/userPostalConfirmation/getAdditionalDocsForCountry.ts
+++
@@ -1,8 +0,0 @@
-import { AnswersHashMap } from "../../../../../../types/AnswersHashMap";
-
-export const getAdditionalDocsForCountry = {
-  Croatia: (_answers: AnswersHashMap, additionalContext) => {
-    const additionalDocs = additionalContext?.additionalDocs ?? [];
-    return [...additionalDocs, "certified copy of passport by a UK lawyer or a public notary"];
-  },
-};
