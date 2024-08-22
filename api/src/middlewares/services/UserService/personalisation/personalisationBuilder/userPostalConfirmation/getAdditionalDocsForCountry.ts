75b36d9cef7a1d66e80f1ffaa3762082cbb061d1
jen <jen@cautionyourblast.com>
2024-08-22 12:14:52 +0100

    remove italy

--- api/src/middlewares/services/UserService/personalisation/personalisationBuilder/userPostalConfirmation/getAdditionalDocsForCountry.ts
+++ api/src/middlewares/services/UserService/personalisation/personalisationBuilder/userPostalConfirmation/getAdditionalDocsForCountry.ts
@@ -1,27 +1,6 @@
 import { AnswersHashMap } from "../../../../../../types/AnswersHashMap";
 
 export const getAdditionalDocsForCountry = {
-  Italy: (answers: AnswersHashMap, _additionalContext) => {
-    const partnerHadPreviousMarriage = answers.partnerMaritalStatus !== "Never married";
-    const livesInItaly = answers.livesInCountry === true;
-
-    const additionalDocs = ["your parents’ full names"];
-
-    if (livesInItaly) {
-      additionalDocs.push("original proof of address or a certified copy you live in Italy");
-    }
-
-    if (!livesInItaly) {
-      additionalDocs.push("original proof of address or a certified copy if your permanent address is outside Italy");
-    }
-
-    if (partnerHadPreviousMarriage) {
-      additionalDocs.push("the document that proves any previous marriages or civil partnerships has ended");
-    }
-
-    return additionalDocs;
-  },
-
   Croatia: (_answers: AnswersHashMap, additionalContext) => {
     const additionalDocs = additionalContext?.additionalDocs ?? [];
     return [...additionalDocs, "certified copy of passport by a UK lawyer or a public notary"];
