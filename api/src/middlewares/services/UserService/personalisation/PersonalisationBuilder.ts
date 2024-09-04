ce4dcfa48e4445039715fa4481b07103668d603e
Luke Zigler <luke@cautionyourblast.com>
2024-09-04 10:53:41 +0100

    Removed post notification from personalisation builder mappings

--- api/src/middlewares/services/UserService/personalisation/PersonalisationBuilder.ts
+++ api/src/middlewares/services/UserService/personalisation/PersonalisationBuilder.ts
@@ -1,13 +1,7 @@
 import { buildUserConfirmationPersonalisation } from "./personalisationBuilder/userConfirmation";
-import { buildPostNotificationPersonalisation } from "./personalisationBuilder.postNotification";
 import { buildUserPostalConfirmationPersonalisation } from "./personalisationBuilder/userPostalConfirmation";
 
 export const PersonalisationBuilder = {
-  /**
-   * Email to send to a post, mentioning that there is a new form to process.
-   */
-  postNotification: buildPostNotificationPersonalisation,
-
   /**
    * Confirmation email for the user, if the user is applying in person, detailing how to book, what to bring etc.
    */
