8ea7cf5cec9060746b2e7e28460d5448e613f8ed
Luke Zigler <luke@cautionyourblast.com>
2024-09-04 16:42:34 +0100

    Added scheduled job for the failure check and a worker to process the new failure check jobs

--- worker/src/queues/notify/workers/index.ts
+++ worker/src/queues/notify/workers/index.ts
@@ -3,10 +3,12 @@ import pino from "pino";
 import PgBoss from "pg-boss";
 import { notifySendHandler } from "./notifySendHandler";
 import { notifyProcessHandler } from "./notifyProcessHandler";
+import { notifyFailureHandler } from "./notifyFailureHandler";
 
 const logger = pino();
 const SEND_QUEUE = "NOTIFY_SEND";
 const PROCESS_QUEUE = "NOTIFY_PROCESS";
+const FAILURE_CHECK_QUEUE = "NOTIFY_FAILURE_CHECK";
 
 export async function setupNotifyWorker() {
   const consumer: PgBoss = await getConsumer();
@@ -16,4 +18,8 @@ export async function setupNotifyWorker() {
 
   logger.info({ PROCESS_QUEUE }, `starting 'notifyProcessHandler' on ${PROCESS_QUEUE} listeners`);
   await consumer.work(PROCESS_QUEUE, { newJobCheckInterval: 2000 }, notifyProcessHandler);
+
+  logger.info({ FAILURE_CHECK_QUEUE }, `starting 'notifyFailureHandler' on ${FAILURE_CHECK_QUEUE} listeners`);
+  await consumer.schedule(FAILURE_CHECK_QUEUE, `0 0 0 * *`);
+  await consumer.work(FAILURE_CHECK_QUEUE, { newJobCheckInterval: 43200000 }, notifyFailureHandler);
 }
