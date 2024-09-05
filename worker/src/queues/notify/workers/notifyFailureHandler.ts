f6c497ad725458704a7a026bcdfdc1d7a60e628d
Luke Zigler <luke@cautionyourblast.com>
2024-09-05 08:24:13 +0100

    Shifted notify response handling logic to getFailureResponses

--- worker/src/queues/notify/workers/notifyFailureHandler.ts
+++ worker/src/queues/notify/workers/notifyFailureHandler.ts
@@ -1,6 +1,6 @@
 import pino from "pino";
 import { Job } from "pg-boss";
-import { NotifyClient, Status, ErrorResponse, GetNotificationsResponse } from "notifications-node-client";
+import { NotifyClient, Status, GetNotificationsResponse, GetNotificationByIdResponse, ErrorResponse } from "notifications-node-client";
 import config from "config";
 import { NOTIFY_FAILURE_ERRORS } from "./errors";
 
@@ -17,54 +17,40 @@ const notifyClient = new NotifyClient(config.get<string>("Notify.apiKey"));
 export async function notifyFailureHandler(job: Job) {
   const jobId = job.id;
   logger.info({ jobId }, `received ${worker} job`);
-  try {
-    const responses = await getFailureResponses();
-    const failures = responses
-      .flatMap((response, index) => {
-        if (response.status !== 200) {
-          logger.warn(`Could not fetch email failures for status: ${failureStatuses[index]}`);
-          return [];
-        }
-        return (response.data as GetNotificationsResponse).notifications;
-      })
-      .filter((failure) => failure);
-    if (failures.length > 0) {
-      failures.forEach((failure) => {
-        logger.warn(
-          {
-            jobId,
-            errorCode: "NOTIFY_EMAIL_FAILURE_TO_SEND",
-          },
-          `Received user confirmation email send error from Notify. Reference: ${failure.reference}`
-        );
-      });
-      return;
-    }
+  const failures = await getFailureResponses();
+  if (failures.length === 0) {
     logger.info({ jobId }, `No email failures found in Notify failure check run.`);
     return;
-  } catch (err) {
-    logger.error(
+  }
+  failures.forEach((failure) => {
+    logger.warn(
       {
         jobId,
-        errorCode: NOTIFY_FAILURE_ERRORS.REQUEST,
-        err,
+        errorCode: "NOTIFY_EMAIL_FAILURE_TO_SEND",
       },
-      `Could not retrieve email failure information from Notify.`
+      `Received user confirmation email send error from Notify. Reference: ${failure.reference}`
     );
-  }
+  });
+  return;
 }
 
 const failureStatuses: Status[] = ["permanent-failure", "temporary-failure", "technical-failure"];
 
-type notifyResponse = {
-  status: number;
-  data: GetNotificationsResponse | ErrorResponse;
-};
-
 async function getFailureResponses() {
-  let responses: notifyResponse[] = [];
+  let responses: GetNotificationByIdResponse[] = [];
   for (const status of failureStatuses) {
-    responses.push(await notifyClient.getNotifications("email", status));
+    try {
+      const { data } = await notifyClient.getNotifications("email", status);
+      if (isGetNotificationsResponse(data)) {
+        responses.concat(data.notifications);
+      }
+    } catch (err) {
+      logger.warn(`Could not fetch email failures for status: ${status}`);
+    }
   }
   return responses;
+}
+
+function isGetNotificationsResponse(response: ErrorResponse | GetNotificationsResponse): response is GetNotificationsResponse {
+  return response.hasOwnProperty("notifications");
 }
