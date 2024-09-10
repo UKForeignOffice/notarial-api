fb424623ac293d7e17dbe84b68a4cee9d00458a8
jen <jen@cautionyourblast.com>
2024-09-10 15:28:34 +0100

    rename staffEmailHandler to caseEmailHandler

--- api/src/routes/forms/router.ts
+++ api/src/routes/forms/router.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import * as formsHandlers from "../../handlers/forms";
-import * as staffEmailHandlers from "../../handlers/forms/emails/staff";
+import * as caseEmailHandlers from "../../handlers/forms/emails/case";
 import * as userEmailHandlers from "../../handlers/forms/emails/user";
 import { validationHandler } from "../../middlewares/validate";
 
 export const formRouter = Router();
 
 formRouter.post("/", validationHandler, formsHandlers.post);
-formRouter.post("/emails/ses", staffEmailHandlers.validate, staffEmailHandlers.post);
+formRouter.post("/emails/ses", caseEmailHandlers.validate, caseEmailHandlers.post);
 formRouter.post("/emails/notify", userEmailHandlers.validate, userEmailHandlers.post);
