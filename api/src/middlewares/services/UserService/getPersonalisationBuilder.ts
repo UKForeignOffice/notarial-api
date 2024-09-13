e376356fb9f0f6836d641eb00a2c98397c06bd27
Luke Zigler <luke@cautionyourblast.com>
2024-09-13 14:50:08 +0100

    Renamed rogue occurrence of MARRIAGE_CASE_TYPES

--- api/src/middlewares/services/UserService/getPersonalisationBuilder.ts
+++ api/src/middlewares/services/UserService/getPersonalisationBuilder.ts
@@ -1,14 +1,14 @@
 import { MarriagePersonalisationBuilder } from "./personalisation/PersonalisationBuilder/marriage/PersonalisationBuilder";
 import { CertifyCopyPersonalisationBuilder } from "./personalisation/PersonalisationBuilder/certifyCopy/PersonalisationBuilder";
 import { FormType } from "../../../types/FormDataBody";
-import { MARRIAGE_CASE_TYPES } from "../../../utils/formTypes";
+import { MARRIAGE_FORM_TYPES } from "../../../utils/formTypes";
 
 const personalisationBuilderMapper = {
   marriage: MarriagePersonalisationBuilder,
   certifyCopy: CertifyCopyPersonalisationBuilder,
 };
 export function getPersonalisationBuilder(type: FormType) {
-  if (MARRIAGE_CASE_TYPES.has(type)) {
+  if (MARRIAGE_FORM_TYPES.has(type)) {
     return personalisationBuilderMapper.marriage;
   }
   return personalisationBuilderMapper[type];
