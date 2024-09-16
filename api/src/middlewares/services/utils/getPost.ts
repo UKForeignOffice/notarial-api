c4ab172920d448317c798515ecc096d3c9118299
Luke Zigler <luke@cautionyourblast.com>
2024-09-16 15:37:34 +0100

    Updated getPost function to choose the right additional context to receive the post from

--- api/src/middlewares/services/utils/getPost.ts
+++ api/src/middlewares/services/utils/getPost.ts
@@ -1,5 +1,11 @@
 import * as additionalContexts from "./additionalContexts.json";
+import { FormType } from "../../../types/FormDataBody";
+import { MARRIAGE_FORM_TYPES } from "../../../utils/formTypes";
 
-export function getPost(country: string, post?: string) {
-  return post ?? additionalContexts.countries?.[country]?.post;
+export function getPost(country: string, type: FormType, post?: string) {
+  let additionalContext: Record<string, any> = { ...additionalContexts };
+  if (!MARRIAGE_FORM_TYPES.has(type)) {
+    additionalContext = additionalContext.certifyCopy;
+  }
+  return post ?? additionalContext.countries?.[country]?.post;
 }
