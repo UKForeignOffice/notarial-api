40984c543afaa1eec86fa84a1d6cdda30584c228
Luke Zigler <luke@cautionyourblast.com>
2024-09-16 15:39:48 +0100

    Updated mappings to match form fields for certify a copy

--- api/src/middlewares/services/CaseService/certifyCopy/mappings.ts
+++ api/src/middlewares/services/CaseService/certifyCopy/mappings.ts
@@ -1,17 +1,11 @@
 export const order = {
   contactDetails: "Contact details",
   applicantDetails: "Applicant's details",
+  childName: "Child's details",
+  childBirth: "Child's birth details",
+  childPassport: "Child's passport details",
   applicantBirth: "Applicant's birth details",
-  applicantAddress: "Applicant's address",
-  occupation: "Occupation",
-  parentDetails: "Parents' details",
   applicantPassport: "Applicant's passport",
-  maritalStatus: "Marital and civil status",
-  partnerName: "Partner's name",
-  partnerAddress: "Partner's address",
-  partnerNationality: "Partner's nationality",
-  partnerMaritalStatus: "Partner's marital and civil status",
-  marriageDetails: "Marriage",
   postalAddress: "Postal address",
   feedbackConsent: "Feedback consent",
 };
@@ -19,12 +13,23 @@ export const order = {
 export const remap = {
   country: "information.country",
   post: "information.post",
-  jurats: "information.jurats",
-  certifyPassport: "information.certifyPassport",
-  oathType: "information.oathType",
+  typeOfDoc: "information.typeOfDocument",
+  applicationType: "information.applicationType",
 
   phoneNumber: "contactDetails.phoneNumber",
   emailAddress: "contactDetails.emailAddress",
+
+  childFirstName: "childName.childFirstName",
+  childMiddleName: "childName.childMiddleName",
+  childSurname: "childName.childSurname",
+  childSex: "childName.childSex",
+
+  childDateOfBirth: "childBirth.dateOfBirth",
+  childPlaceOfBirth: "childBirth.placeOfBirth",
+  childCountryOfBirth: "childBirth.countryOfBirth",
+
+  childPassportNumber: "applicantPassport.number",
+  childPassportDateOfIssue: "applicantPassport.dateOfIssue",
 
   firstName: "applicantDetails.firstName",
   middleName: "applicantDetails.middleName",
@@ -34,55 +39,9 @@ export const remap = {
   dateOfBirth: "applicantBirth.dateOfBirth",
   placeOfBirth: "applicantBirth.placeOfBirth",
   countryOfBirth: "applicantBirth.countryOfBirth",
-  nameChangedByDeedPoll: "applicantBirth.nameChangedByDeedPoll",
-  previousNameByDeedPoll: "applicantBirth.previousNameByDeedPoll",
-  nameChangedByMarriage: "applicantBirth.nameChangedByMarriage",
-  previousNameByMarriage: "applicantBirth.previousNameByMarriage",
-
-  addressLine1: "applicantAddress.addressLine1",
-  addressLine2: "applicantAddress.addressLine2",
-  city: "applicantAddress.city",
-  postcode: "applicantAddress.postcode",
-  countryAddress: "applicantAddress.country",
-
-  occupation: "occupation.occupation",
-
-  fatherFullName: "parentDetails.fatherFullName",
-  motherFullName: "parentDetails.motherFullName",
-  motherMaidenName: "parentDetails.motherMaidenName",
 
   passportNumber: "applicantPassport.number",
   passportDateOfIssue: "applicantPassport.dateOfIssue",
-
-  maritalStatus: "maritalStatus.status",
-  dateOfDecreeAbsolute: "maritalStatus.dateOfDecreeAbsolute",
-  placeOfDecreeAbsolute: "maritalStatus.placeOfDecreeAbsolute",
-  dateOfFinalOrder: "maritalStatus.dateOfFinalOrder",
-  placeOfFinalOrder: "maritalStatus.placeOfFinalOrder",
-  dateOfPartnerDeathCert: "maritalStatus.dateOfPartnerDeathCert",
-  placeOfPartnerDeathCert: "maritalStatus.placeOfPartnerDeathCert",
-  dateOfDissolution: "maritalStatus.dateOfDissolution",
-  placeOfDissolution: "maritalStatus.placeOfDissolution",
-  dateOfDecreeOfNullity: "maritalStatus.dateOfDecreeOfNullity",
-  placeOfDecreeOfNullity: "maritalStatus.placeOfDecreeOfNullity",
-
-  partnerFirstName: "partnerName.partnerFirstName",
-  partnerMiddleName: "partnerName.partnerMiddleName",
-  partnerSurname: "partnerName.partnerSurname",
-  partnerSex: "partnerName.partnerSex",
-
-  partnerAddressLine1: "partnerAddress.partnerAddressLine1",
-  partnerAddressLine2: "partnerAddress.partnerAddressLine2",
-  partnerAddressCity: "partnerAddress.partnerAddressCity",
-  partnerAddressPostcode: "partnerAddress.partnerAddressPostcode",
-  partnerAddressCountry: "partnerAddress.partnerAddressCountry",
-
-  partnerNationality: "partnerNationality.partnerNationality",
-
-  partnerMaritalStatus: "partnerMaritalStatus.partnerMaritalStatus",
-
-  dateOfMarriage: "marriageDetails.dateOfMarriage",
-  placeOfMarriage: "marriageDetails.placeOfMarriage",
 
   deliveryAddressLine1: "postalAddress.addressLine1",
   deliveryAddressLine2: "postalAddress.addressLine2",
