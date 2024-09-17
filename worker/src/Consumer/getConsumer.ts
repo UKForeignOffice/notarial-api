d19c973fb57876165a60dcfdaf12559ac7a506cd
jen <jen@cautionyourblast.com>
2024-09-17 11:01:24 +0100

    add back monitorStateIntervalSeconds

--- worker/src/Consumer/getConsumer.ts
+++ worker/src/Consumer/getConsumer.ts
@@ -33,6 +33,7 @@ export async function create(options: ConsumerOptions) {
     connectionString,
     archiveFailedAfterSeconds: archiveFailedAfterDays * DAY_IN_S,
     deleteAfterDays,
+    monitorStateIntervalSeconds,
     schema,
   });
 
