dc647c2ff6925f03978193aeef9b29e3221f8324
Luke Zigler <luke@cautionyourblast.com>
2024-09-17 13:54:30 +0100

    removed references to CaseServiceBase from CaseService type

--- api/src/middlewares/services/CaseService/types/CaseService.ts
+++ api/src/middlewares/services/CaseService/types/CaseService.ts
@@ -5,10 +5,9 @@ import { FormType, PayMetadata } from "../../../../types/FormDataBody";
 import { SESSendJob } from "./SESSendJob";
 import { ProcessQueueData } from "./ProcessQueueData";
 import { PaymentViewModel } from "./PaymentViewModel";
-import { CaseServiceBaseType } from "./CaseServiceBase";
 import { ProcessQueueDataInput } from "./ProcessQueueDataInput";
 
-export interface CaseService extends CaseServiceBaseType {
+export interface CaseService {
   queueService: QueueService;
 
   sendToProcessQueue(data: ProcessQueueData): Promise<string>;
@@ -21,15 +20,15 @@ export interface CaseService extends CaseServiceBaseType {
   getEmailBody(data: { fields: FormField[]; payment?: PaymentViewModel; reference: string }, type: FormType): string;
 
   /**
+   * builds the data required for the SES_PROCESS job
+   */
+  buildProcessQueueData(input: ProcessQueueDataInput): ProcessQueueData;
+
+  /**
    * Parses the payment data in metadata
    * TODO:- refactor into own class or function
    */
   paymentViewModel(payment: PayMetadata | undefined, country: string): PaymentViewModel | undefined;
-
-  /**
-   * builds the data required for the SES_PROCESS job
-   */
-  buildProcessQueueData(input: ProcessQueueDataInput): ProcessQueueData;
 
   /**
    * builds the data required for the SES_SEND job
