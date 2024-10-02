dc904ea50c626ac0420eb93ac78db6cbb93a7fcc
Luke Zigler <luke@cautionyourblast.com>
2024-10-02 16:47:32 +0100

    Updated getApplicationTypeName to be marriage specific for now

--- api/src/middlewares/services/CaseService/utils/getApplicationTypeName.ts
+++ api/src/middlewares/services/CaseService/utils/getApplicationTypeName.ts
@@ -1,14 +1,13 @@
-import { FormType } from "../../../../types/FormDataBody";
+import { MarriageFormType } from "../../../../types/FormDataBody";
 
-const applicationTypeNames: Record<FormType, string> = {
+const applicationTypeNames: Record<MarriageFormType, string> = {
   affirmation: "a marital status affirmation",
   cni: "a notice of marriage and marital status affirmation",
   exchange: "to exchange a UK issued Certificate of No Impediment for a local notice of marriage",
   msc: "a marital status certificate",
   cniAndMsc: "a notice of marriage and marital status certificate",
-  certifyCopy: "",
 };
 
-export function getApplicationTypeName(type: FormType) {
+export function getApplicationTypeName(type: MarriageFormType) {
   return applicationTypeNames[type];
 }
