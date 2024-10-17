49a0a73cfacf0c060c8effbe3b26f3a38128db2d
jen <jen@cautionyourblast.com>
2024-10-17 11:48:45 +0100

    add requestDocumentUserTemplates

--- api/src/middlewares/services/UserService/NotifyTemplates/UserTemplates.ts
+++ api/src/middlewares/services/UserService/NotifyTemplates/UserTemplates.ts
@@ -1,29 +1,22 @@
 import config from "config";
-import { AnswersHashMap, RequestDocumentAnswersHashmap } from "../../../../types/AnswersHashMap";
+import { AnswersHashMap } from "../../../../types/AnswersHashMap";
 import { FormType, MarriageFormType, PayMetadata } from "../../../../types/FormDataBody";
 import { MARRIAGE_FORM_TYPES } from "../../../../utils/formTypes";
-import { getPersonalisationBuilder } from "../getPersonalisationBuilder";
-import * as additionalContexts from "../../utils/additionalContexts.json";
 import { MarriageUserTemplates } from "./MarriageUserTemplates";
 import { CertifyCopyUserTemplates } from "./CertifyCopyUserTemplates";
+import { RequestDocumentUserTemplates } from "./RequestDocumentUserTemplates";
+import { ApplicationError } from "../../../../ApplicationError";
 
 export class UserTemplates {
   marriage: MarriageUserTemplates;
   certifyCopy: CertifyCopyUserTemplates;
-  templates = {
-    certifyCopy: {},
-    requestDocument: {
-      "USA - J1 visa no objection statement": config.get<string>("Notify.Template.requestDocument.J1"),
-      "Democratic Republic of the Congo - consular certificate": config.get<string>("Notify.Template.requestDocument.appointment"),
-      appointment: config.get<string>("Notify.Template.requestDocument.appointment"),
-      courier: config.get<string>("Notify.Template.requestDocument.courier"),
-      posted: config.get<string>("Notify.Template.requestDocument.posted"),
-    },
-  };
+  requestDocument: RequestDocumentUserTemplates;
+
   constructor() {
     try {
       this.marriage = new MarriageUserTemplates();
       this.certifyCopy = new CertifyCopyUserTemplates();
+      this.requestDocument = new RequestDocumentUserTemplates();
     } catch (e) {
       console.error("Notify templates have not been configured, exiting", e);
       process.exit(1);
@@ -31,71 +24,24 @@ export class UserTemplates {
   }
 
   getTemplate(data: { answers: AnswersHashMap; metadata: { reference: string; payment?: PayMetadata; type: FormType; postal?: boolean } }) {
-    const { answers, metadata } = data;
     const { type } = data.metadata;
 
-    if (MARRIAGE_FORM_TYPES.has(metadata.type)) {
+    if (this.isMarriageFormType(type)) {
       return this.marriage.getTemplate(data);
     }
 
-    if (metadata.type === "certifyCopy") {
+    if (type === "certifyCopy") {
       return this.certifyCopy.getTemplate(data);
     }
 
-    let isPostalApplication = metadata.postal;
-
-    const postalVariant = this.getPostalVariant(answers, isPostalApplication, type);
-    let template, builder;
-
     if (type === "requestDocument") {
-      const requestDocAnswers = answers as RequestDocumentAnswersHashmap;
-      const documentType = requestDocAnswers.serviceType;
-
-      if (documentType === "No objection certificate to adopt a child") {
-        const country = requestDocAnswers.applicationCountry;
-        const adoptionTemplateName = getAdoptionTemplateName(country!);
-        template = this.templates.requestDocument[adoptionTemplateName];
-      }
-
-      template ??= this.templates.requestDocument[documentType];
-
-      builder = getPersonalisationBuilder(type);
+      return this.requestDocument.getTemplate(data);
     }
 
-    template ??= this.templates[type][postalVariant];
-
-    const personalisationBuilder = getPersonalisationBuilder(type);
-
-    builder ??= personalisationBuilder[postalVariant];
-
-    return {
-      template,
-      personalisationBuilder: builder,
-    };
+    throw new ApplicationError("NOTIFY", "UNKNOWN", 500, `Could not find the right template or builder for ${type}`);
   }
 
-  getPostalVariant(answers: AnswersHashMap, postal: boolean | undefined, type: FormType) {
-    const country = answers.country as string;
-    // for exchange forms, any country that offers a postal journey and cni delivery should be a postal application.
-    const countryOffersPostalRoute = additionalContexts.marriage.countries[country]?.postal && additionalContexts.marriage.countries[country]?.cniDelivery;
-    // Croatia is an exception to this, and only offers in-person applications for exchange
-    const countryIsCroatia = country === "Croatia";
-
-    const postalSupport = postal ?? (type === "exchange" && countryOffersPostalRoute && !countryIsCroatia);
-
-    return postalSupport ? "postal" : "inPerson";
+  isMarriageFormType(type: FormType): type is MarriageFormType {
+    return MARRIAGE_FORM_TYPES.has(type);
   }
-}
-
-type GenericRequestDocumentTemplates = "courier" | "appointment" | "posted";
-function getAdoptionTemplateName(country: string) {
-  const countryMap: { [key: string]: GenericRequestDocumentTemplates } = {
-    India: "courier",
-    Vietnam: "appointment",
-    Spain: "posted",
-    "United Arab Emirates": "appointment",
-    Thailand: "posted",
-  };
-
-  return countryMap[country];
 }
--- api/src/middlewares/services/UserService/personalisation/PersonalisationBuilder/requestDocument/index.ts
+++ api/src/middlewares/services/UserService/personalisation/PersonalisationBuilder/requestDocument/index.ts
@@ -1,16 +1,16 @@
-import { RequestDocumentAnswersHashmap } from "../../../../../../types/AnswersHashMap";
-import { PayMetadata, RequestDocumentFormType } from "../../../../../../types/FormDataBody";
+import { AnswersHashMap, RequestDocumentAnswersHashmap } from "../../../../../../types/AnswersHashMap";
+import { FormType, PayMetadata } from "../../../../../../types/FormDataBody";
 import { ApplicationError } from "../../../../../../ApplicationError";
 import { getPostForRequestDocument } from "../../../../utils/getPost";
 
-export function requestDocumentPersonalisationBuilder(
-  answers: RequestDocumentAnswersHashmap,
-  metadata: { reference: string; payment?: PayMetadata; type?: RequestDocumentFormType }
-) {
+export function requestDocumentPersonalisationBuilder(answers: AnswersHashMap, metadata: { reference: string; payment?: PayMetadata; type?: FormType }) {
   if (!answers) {
     throw new ApplicationError("WEBHOOK", "VALIDATION", 500, "Fields are empty");
   }
-  const post = getPostForRequestDocument(answers.serviceType, answers.applicationCountry, answers.post);
+
+  const requestDocumentAnswers = answers as RequestDocumentAnswersHashmap;
+  const { serviceType, applicationCountry, post: answersPost } = requestDocumentAnswers;
+  const post = getPostForRequestDocument(serviceType, applicationCountry, answersPost);
 
   return {
     post,
