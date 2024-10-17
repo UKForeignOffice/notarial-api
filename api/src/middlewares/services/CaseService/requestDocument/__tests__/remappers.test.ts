a5209dd24a30c2e051ccc5d6922294c24c7e75a5
jen <jen@cautionyourblast.com>
2024-10-17 10:34:10 +0100

    feat: add adoption SES tests and DRC user email

--- api/src/middlewares/services/CaseService/requestDocument/__tests__/remappers.test.ts
+++ api/src/middlewares/services/CaseService/requestDocument/__tests__/remappers.test.ts
@@ -1,6 +1,6 @@
 import { flattenQuestions } from "../../../helpers";
 import { requestDocumentRemapper, requestDocumentReorderer } from "../helpers";
-import { data } from "./fixtures/data";
+import { data, fields } from "./fixtures/data";
 
 test("requestDocument is successfully remapped", () => {
   const flattened = flattenQuestions(data.data.questions);
@@ -13,4 +13,14 @@ test("requestDocument is successfully reordered", () => {
   const remapped = requestDocumentRemapper(flattened);
   const reordered = requestDocumentReorderer(remapped);
   expect(reordered).toMatchSnapshot();
+});
+
+describe("requestDocument - adoption letter - unique fields are included", () => {
+  test("remapper", () => {
+    const remapped = requestDocumentRemapper(fields.adoption);
+    expect(remapped.partnerDetails.partnerFirstName).toBeDefined();
+    expect(remapped.partnerDetails.partnerIsBN).toBeDefined();
+    expect(remapped.partnerDetails.partnerPassportNumber).toBeDefined();
+    expect(remapped.partnerDetails.partnerCountryOfBirth).toBeDefined();
+  });
 });
