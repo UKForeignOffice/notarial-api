0be67ad5cda85faa9579b1c7387d4e05cc80c088
Luke Zigler <luke@cautionyourblast.com>
2024-10-31 10:13:13 +0000

    Moved mappings to the correct remapper

--- api/src/middlewares/services/CaseService/requestDocument/mappings.ts
+++ api/src/middlewares/services/CaseService/requestDocument/mappings.ts
@@ -3,7 +3,6 @@ export const order = {
   applicantPassport: "Applicant's passport details",
   permanentAddress: "Applicant's permanent address",
   contactDetails: "Contact details",
-  parent2ContactDetails: "Parent 2 contact details",
   reason: "Reason for document",
   certificate: "Certificate details",
   maritalStatus: "Marital status",
@@ -69,7 +68,4 @@ export const remap = {
 
   emailAddress: "contactDetails.emailAddress",
   phoneNumber: "contactDetails.phoneNumber",
-
-  "parent2Contact.emailAddress": "parent2ContactDetails.emailAddress",
-  "parent2Contact.phoneNumber": "parent2ContactDetails.phoneNumber",
 };
