5adf2ce191b4b71d0d4e30b787b9297d1ef14a5a
Aadil Ahmed <aadil.ahmed@kainos.com>
2025-05-30 10:01:41 +0100

    Revert "send base64 string instead of Buffer for messageInstance.attach"
    This reverts commit 0478a66dec00acbecda9e739c921e8816bf9ea18.

--- worker/src/queues/ses/helpers/SESEmail.ts
+++ worker/src/queues/ses/helpers/SESEmail.ts
@@ -47,7 +47,7 @@ export async function attachFilesToMessage(attachments: FormField[], messageInst
     messageInstance.attach({
       filename: `${attachment.title}.${FileMimeType[contentType]}`,
       contentType,
-      data: data.toString("base64"),
+      data: Buffer.from(data, 'base64'),
     });
   }
   return messageInstance;
